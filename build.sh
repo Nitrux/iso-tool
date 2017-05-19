4c45f5af6ef9cd9c5810f86f5bf172c79aca971a
luis-lavaire <llavaire01@gmail.com>
2017-05-19 17:41:52 -0600

    28143

--- build.sh
+++ build.sh
@@ -12,70 +12,62 @@ out_dir=out
 
 script_path=$(readlink -f ${0%/*})
 
-# Helper function to run make_*() only one time per architecture.
-run_once() {
-    if [[ ! -e ${work_dir}/build.${1} ]]; then
-        $1
-        touch ${work_dir}/build.${1}
-    fi
-}
-
-# Base installation (airootfs)
+# Base installation (nxos)
 make_basefs() {
-    mkarchiso -v -w "${work_dir}" -D "${install_dir}" init
+	mkarchiso -v -w "${work_dir}" -D "${install_dir}" init
 }
 
-# Copy mkinitcpio archiso hooks and build initramfs (airootfs)
+# Copy mkinitcpio archiso hooks and build initramfs (nxos)
 make_setup_mkinitcpio() {
-    mkdir -p ${work_dir}/airootfs/etc/initcpio/hooks
-    mkdir -p ${work_dir}/airootfs/etc/initcpio/install
-    cp /usr/lib/initcpio/hooks/archiso ${work_dir}/airootfs/etc/initcpio/hooks
-    cp /usr/lib/initcpio/install/archiso ${work_dir}/airootfs/etc/initcpio/install
-    cp ${script_path}/mkinitcpio.conf ${work_dir}/airootfs/etc/mkinitcpio-archiso.conf
-    mkarchiso -v -w "${work_dir}" -D "${install_dir}" -r 'mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/nxos.img' run
+	mkdir -p ${work_dir}/etc/initcpio/hooks
+	mkdir -p ${work_dir}/etc/initcpio/install
+	cp /usr/lib/initcpio/hooks/archiso ${work_dir}/etc/initcpio/hooks
+	cp /usr/lib/initcpio/install/archiso ${work_dir}/etc/initcpio/install
+	cp ${script_path}/mkinitcpio.conf ${work_dir}/etc/mkinitcpio-archiso.conf
+	mkarchiso -v -w "${work_dir}" -D "${install_dir}" -r 'mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/nxos.img' run
 }
 
 # Prepare ${install_dir}/boot/
 make_boot() {
-    mkdir -p ${work_dir}/iso/${install_dir}/boot
-    cp ${work_dir}/airootfs/boot/archiso.img ${work_dir}/iso/${install_dir}/boot/archiso.img
-    cp ${work_dir}/airootfs/boot/vmlinuz-linux ${work_dir}/iso/${install_dir}/boot/vmlinuz
+	mkdir -p ${work_dir}/iso/${install_dir}/boot
+	cp ${work_dir}/boot/nxos.img ${work_dir}/iso/${install_dir}/boot/nxos.img
+	cp ${work_dir}/boot/vmlinuz-linux ${work_dir}/iso/${install_dir}/boot/vmlinuz
 }
 
 # Prepare /${install_dir}/boot/syslinux
 make_syslinux() {
-    mkdir -p ${work_dir}/iso/${install_dir}/boot/syslinux
-    sed "s|%ARCHISO_LABEL%|${iso_label}|g;
-         s|%INSTALL_DIR%|${install_dir}|g;
-         s|%ARCH%|${arch}|g" ${script_path}/syslinux/syslinux.cfg > ${work_dir}/iso/boot/syslinux/syslinux.cfg
-    cp ${work_dir}/airootfs/usr/lib/syslinux/bios/ldlinux.c32 ${work_dir}/iso/boot/syslinux/
-    cp ${work_dir}/airootfs/usr/lib/syslinux/bios/menu.c32 ${work_dir}/iso/boot/syslinux/
-    cp ${work_dir}/airootfs/usr/lib/syslinux/bios/libutil.c32 ${work_dir}/iso/boot/syslinux/
+	mkdir -p ${work_dir}/iso/${install_dir}/boot/syslinux
+	sed "s|%ARCHISO_LABEL%|${iso_label}|g;
+		 s|%INSTALL_DIR%|${install_dir}|g;
+		 s|%ARCH%|${arch}|g" ${script_path}/syslinux/syslinux.cfg > ${work_dir}/iso/boot/syslinux/syslinux.cfg
+	cp ${work_dir}/usr/lib/syslinux/bios/ldlinux.c32 ${work_dir}/iso/boot/syslinux/
+	cp ${work_dir}/usr/lib/syslinux/bios/menu.c32 ${work_dir}/iso/boot/syslinux/
+	cp ${work_dir}/usr/lib/syslinux/bios/libutil.c32 ${work_dir}/iso/boot/syslinux/
 }
 
 # Prepare /isolinux
 make_isolinux() {
-    mkdir -p ${work_dir}/iso/isolinux
-    sed "s|%INSTALL_DIR%|${install_dir}|g" ${script_path}/isolinux/isolinux.cfg > ${work_dir}/iso/isolinux/isolinux.cfg
-    cp ${work_dir}/airootfs/usr/lib/syslinux/bios/isolinux.bin ${work_dir}/iso/isolinux/
-    cp ${work_dir}/airootfs/usr/lib/syslinux/bios/isohdpfx.bin ${work_dir}/iso/isolinux/
-    cp ${work_dir}/airootfs/usr/lib/syslinux/bios/ldlinux.c32 ${work_dir}/iso/isolinux/
+	mkdir -p ${work_dir}/iso/isolinux
+	sed "s|%INSTALL_DIR%|${install_dir}|g" ${script_path}/isolinux/isolinux.cfg > ${work_dir}/iso/isolinux/isolinux.cfg
+	cp ${work_dir}/usr/lib/syslinux/bios/isolinux.bin ${work_dir}/iso/isolinux/
+	cp ${work_dir}/usr/lib/syslinux/bios/isohdpfx.bin ${work_dir}/iso/isolinux/
+	cp ${work_dir}/usr/lib/syslinux/bios/ldlinux.c32 ${work_dir}/iso/isolinux/
 }
 
-# Build airootfs filesystem image
+# Build nxos filesystem image
 make_prepare() {
-    mkarchiso -v -w "${work_dir}" -D "${install_dir}" prepare
+	mkarchiso -v -w "${work_dir}" -D "${install_dir}" prepare
 }
 
 # Build ISO
 make_iso() {
-    mkarchiso -v -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}-${iso_version}-${arch}.iso"
+	mkarchiso -v -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}.iso"
 }
 
-#run_once make_basefs
-#run_once make_setup_mkinitcpio
-#run_once make_boot
-#run_once make_syslinux
-run_once make_isolinux
-run_once make_prepare
-run_once make_iso
+make_basefs
+#make_setup_mkinitcpio
+#make_boot
+#make_syslinux
+#make_isolinux
+#make_prepare
+#make_iso
