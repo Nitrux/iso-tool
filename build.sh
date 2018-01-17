726886124d5ce8ead1ba7d8f09d06f8917b6e58c
luis-lavaire <llavaire01@gmail.com>
2018-01-17 17:23:15 -0600

    Commit.

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
 #! /bin/sh
 
-# Build.
-
 wget -q http://archive.ubuntu.com/ubuntu/dists/zesty/main/installer-amd64/current/images/netboot/mini.iso
 wget -q http://repo.nxos.org/dists/nxos/main/binary-amd64/Packages nomad-packages
 
 grep 'Filename: ' nomad-packages > urls.txt
 
-mkdir newroot
+mkdir packages
 for package in $(cat urls.txt); do
 	wget -q http://repo.nxos.org/$package 
-	dpkg -x ${package##*/} newroot/
+	dpkg -x ${package##*/} packages/
 done
+
+wget -q https://github.com/NXOS/busybox/releases/download/continuous/busybox packages/usr/bin/busybox
+chmod +x busybox
 
 mkdir mnt
 mount mini.iso mnt
 
-mkdir iso
+mkdir upper work iso
 
+mount -t overlay -o lowerdir=packages:mnt,upperdir=upper,workdir=work overlay iso
+
+mksquashfs iso rootfs.sfs -comp xz
