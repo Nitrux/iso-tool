474ec4f975f18f9cf7c64d3d4711c6f969ba3f20
luis-lavaire <llavaire01@gmail.com>
2018-01-19 16:37:25 -0600

    Commit.

--- build.sh
+++ build.sh
@@ -21,21 +21,25 @@ for p in $(grep -e 'Filename:.*' Packages | sed 's/Filename: //'); do
 	sudo dpkg -x packages/${p##*/} edit
 done
 
-sudo rm -rf edit/tmp/* edit/vmlinuz edit/initrd.img edit/boot
+as_root () {
+	rm -rf edit/tmp/* edit/vmlinuz edit/initrd.img edit/boot
 
-sudo chmod +w extract-cd/casper/filesystem.manifest
-sudo chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > extract-cd/casper/filesystem.manifest
-sudo cp extract-cd/casper/filesystem.manifest extract-cd/casper/filesystem.manifest-desktop
-sudo sed -i '/ubiquity/d' extract-cd/casper/filesystem.manifest-desktop
-sudo sed -i '/casper/d' extract-cd/casper/filesystem.manifest-desktop
+	chmod +w extract-cd/casper/filesystem.manifest
+	chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > extract-cd/casper/filesystem.manifest
+	cp extract-cd/casper/filesystem.manifest extract-cd/casper/filesystem.manifest-desktop
+	sed -i '/ubiquity/d' extract-cd/casper/filesystem.manifest-desktop
+	sed -i '/casper/d' extract-cd/casper/filesystem.manifest-desktop
 
-sudo rm extract-cd/casper/filesystem.squashfs
-sudo mksquashfs edit extract-cd/casper/filesystem.squashfs -comp xz -e edit/boot
-sudo printf $(du -sx --block-size=1 edit | cut -f 1) > extract-cd/casper/filesystem.size
+	rm extract-cd/casper/filesystem.squashfs
+	mksquashfs edit extract-cd/casper/filesystem.squashfs -comp xz
+	printf $(sudo du -sx --block-size=1 edit | cut -f 1) > extract-cd/casper/filesystem.size
 
-cd extract-cd
-sudo sed -i 's/DISKNAME.*/DISKNAME Nitrux 1.0.8 \"SolarStorm\" - Release amd64/g' README.diskdefines
-sudo rm md5sum.txt
+	cd extract-cd
+	sed -i 's/DISKNAME.*/DISKNAME Nitrux 1.0.8 \"SolarStorm\" - Release amd64/g' README.diskdefines
+	rm md5sum.txt
 
-sudo find -type f -print 0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee md5sum.txt
-sudo mkisofs -D -r -V "Nitrux Live" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../out/Nitrux-1.0.8-SolarStorm.iso .
+	find -type f -print 0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee md5sum.txt
+	mkisofs -D -r -V "Nitrux Live" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../out/Nitrux-1.0.8-SolarStorm.iso .
+}
+
+sudo as_root
