057fb92f18ecebbac09b8b73c68adbe43b157037
luis-lavaire <llavaire01@gmail.com>
2018-01-24 14:16:34 -0600

    Don't complain.

--- build.sh
+++ build.sh
@@ -20,13 +20,13 @@ echo deb http://repo.nxos.org nxos main >> /etc/apt/sources.list
 echo deb http://repo.nxos.org xenial main >> /etc/apt/sources.list
 echo deb http://repo.nxos.org xenial main >> edit/etc/apt/sources.list
 
-apt-get -qq -y update
+apt-get -qq -y --allow-unauthenticated update
 
 wget -q http://repo.nxos.org/public.key -O edit/key
 chroot edit/ sudo apt-key add key
 
-apt-get -o Dir=edit/ -qq -y update
-apt-get -o Dir=edit/ -qq -y install rfkill systemd-sysv librsvg2 nxos-desktop
+apt-get -o Dir=edit/ -qq -y --allow-unauthenticated update
+apt-get -o Dir=edit/ -qq -y --allow-unauthenticated install rfkill systemd-sysv librsvg2-dev nxos-desktop
 
 rm -rf edit/tmp/* edit/vmlinuz edit/initrd.img edit/boot/
 chmod +w extract-cd/casper/filesystem.manifest
