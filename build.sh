d9a2e29a4bc7a753c1635a7e26e038bbc18cecb2
luis-lavaire <llavaire01@gmail.com>
2018-01-24 13:14:14 -0600

    Commit.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
+#(for c in $(seq 50); do echo $c; sleep 60; done) &
+
 echo "Downloading base system..."
-wget -q http://cdimage.ubuntu.com/kubuntu/releases/17.10.1/release/kubuntu-17.10.1-desktop-i386.iso -O os.iso
-wget -q http://repo.nxos.org/dists/nxos/main/binary-amd64/Packages
+wget http://cdimage.ubuntu.com/kubuntu/releases/17.10.1/release/kubuntu-17.10.1-desktop-i386.iso -O os.iso
 
 mkdir mnt out extract-cd lower upper work edit packages
 mount os.iso mnt
@@ -11,8 +12,6 @@ rsync --exclude=/casper/filesystem.squashfs -a mnt/ extract-cd
 
 mount mnt/casper/filesystem.squashfs lower
 mount -t overlay -o lowerdir=lower,upperdir=upper,workdir=work none edit
-
-# Install the nomad-deskop files.
 
 echo "Downloading Nomad packages..."
 
@@ -23,33 +22,28 @@ chroot edit/ sh -c 'wget -qO - http://repo.nxos.org/public.key | sudo apt-key ad
 chroot edit/ apt-get update
 chroot edit/ apt-get install nxos-desktop
 
-#for p in $(grep -e 'Filename:.*' Packages | sed 's/Filename: //'); do
-#	wget -q http://repo.nxos.org/$p -O packages/${p##*/}
-#	dpkg -x packages/${p##*/} edit
-#done
-
-rm -rf edit/tmp/* edit/vmlinuz edit/initrd.img edit/boot
+rm -rf edit/tmp/* edit/vmlinuz edit/initrd.img edit/boot/
 chmod +w extract-cd/casper/filesystem.manifest
 chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > extract-cd/casper/filesystem.manifest
 cp extract-cd/casper/filesystem.manifest extract-cd/casper/filesystem.manifest-desktop
 sed -i '/ubiquity/d' extract-cd/casper/filesystem.manifest-desktop
 sed -i '/casper/d' extract-cd/casper/filesystem.manifest-desktop
-(for c in $(seq 50); do echo $c; sleep 60; done) &
+
 mksquashfs edit extract-cd/casper/filesystem.squashfs -comp xz -noappend -no-progress
 printf $(du -sx --block-size=1 edit | cut -f 1) > extract-cd/casper/filesystem.size
+
 cd extract-cd
-sed -i 's/#define DISKNAME.*/DISKNAME Nitrux 1.0.8 CONTINUOUS - Release amd64/' README.diskdefines
+sed -i 's/#define DISKNAME.*/DISKNAME Nitrux 1.0.8 "NXOS" - Release amd64/' README.diskdefines
 rm md5sum.txt
-rm -rf pics/
+
 find -type f -print0 | xargs -0 md5sum | grep -v isolinux/boot.cat | tee md5sum.txt
 
-xorriso -as mkisofs -r -V "Custom Ubuntu Install CD" \
+xorriso -as mkisofs -r -V "Nitrux Live" \
             -cache-inodes \
             -J -l -b isolinux/isolinux.bin \
             -c isolinux/boot.cat -no-emul-boot \
             -isohybrid-mbr /usr/lib/syslinux/bios/isohdpfx.bin \
             -eltorito-alt-boot \
-            -e boot/grub/efi.img \
             -isohybrid-gpt-basdat \
             -boot-load-size 4 -boot-info-table \
             -o ../out/os.iso ./
