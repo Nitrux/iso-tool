a9b30ea57053985300d58f4fddc8d402532d100f
Luis Lavaire <llavaire01@gmail.com>
2018-01-26 13:30:57 -0600

    Use Ubuntu 16.04

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "Downloading base system..."
-wget -q http://cdimage.ubuntu.com/kubuntu/releases/17.10.1/release/kubuntu-17.10.1-desktop-i386.iso -O os.iso
+wget -q http://cdimage.ubuntu.com/kubuntu/releases/16.04.3/release/kubuntu-16.04-desktop-amd64.iso
 
 mkdir mnt out extract-cd lower upper work edit packages
 mount os.iso mnt
@@ -17,14 +17,10 @@ echo deb http://repo.nxos.org nxos main >> edit/etc/apt/sources.list
 echo deb http://repo.nxos.org xenial main >> edit/etc/apt/sources.list
 
 cp /etc/resolv.conf edit/etc/
-cp /etc/resolvconf.conf edit/etc/
-
-chroot edit/ ping google.com -c 1 || exit 1
 
 chroot edit/ sh -c 'wget -qO - http://repo.nxos.org/public.key | apt-key add -'
-chroot edit/ sh -c 'apt-key list | grep Nitrux'
-chroot edit/ apt-get -y update
-chroot edit/ apt-get -y install nxos-desktop
+chroot edit/ sh -c 'apt-get -y update'
+chroot edit/ sh -c 'apt-get -y install nxos-desktop'
 
 rm -rf edit/tmp/* edit/vmlinuz edit/initrd.img edit/boot/
 chmod +w extract-cd/casper/filesystem.manifest
