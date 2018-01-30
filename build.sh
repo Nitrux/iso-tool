11ceb8a098853dbdda80baaa14379150a2608ec8
Luis Lavaire <llavaire01@gmail.com>
2018-01-29 20:56:24 -0600

    Clean /var until dpkg doesn't need it.

--- build.sh
+++ build.sh
@@ -29,43 +29,49 @@ echo deb http://archive.neon.kde.org/user xenial main >> base/etc/apt/sources.li
 
 cp /etc/resolv.conf base/etc/
 
-chroot base/ sh -c "apt-get install -y busybox-static"
-chroot base/ sh -c "busybox wget -qO - http://repo.nxos.org/public.key | apt-key add -"
-chroot base/ sh -c "busybox wget -qO - http://origin.archive.neon.kde.org/public.key | apt-key add -"
-chroot base/ sh -c "apt-get -y update"
-chroot base/ sh -c "apt-get -y upgrade"
-chroot base/ sh -c "apt-get -y install $PACKAGES"
-chroot base/ sh -c "apt-get -y autoremove"
-chroot base/ sh -c "apt-get -y clean"
+chroot base/ sh -c "
+export HOME=/root
+LANG=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+apt-get install -y busybox-static
+busybox wget -qO - http://repo.nxos.org/public.key | apt-key add -
+busybox wget -qO - http://origin.archive.neon.kde.org/public.key | apt-key add -
+apt-get -y update
+apt-get -y install $PACKAGES
+apt-get -y autoremove
+apt-get -y clean"
 
 
 # Clean things a little.
 
-rm -rf base/tmp/* base/vmlinuz* base/initrd.img* base/boot/ base/var/*
 chmod +w iso/casper/filesystem.manifest
-chroot base/ dpkg-query -W --showformat='${Package} ${Version}\n' > iso/casper/filesystem.manifest
+chroot base/ dpkg-query -W --showformat='${Package} ${Version}\n' | sort -nr > iso/casper/filesystem.manifest
 cp iso/casper/filesystem.manifest iso/casper/filesystem.manifest-desktop
 sed -i '/ubiquity/d' iso/casper/filesystem.manifest-desktop
 sed -i '/casper/d' iso/casper/filesystem.manifest-desktop
+rm -rf base/tmp/* base/vmlinuz* base/initrd.img* base/boot/ base/var/lib/dbus/machine-id
 
 
 # Compress the new filesystem.
 
 echo "Compressing the new filesystem"
 mksquashfs base/ iso/casper/filesystem.squashfs -comp xz -noappend -no-progress
-printf $(du -sx --block-size=1 base | cut -f 1) > iso/casper/filesystem.size
+printf $(du -sx --block-size=1 base/ | cut -f 1) > iso/casper/filesystem.size
 
 cd iso
 sed -i 's/#define DISKNAME.*/DISKNAME Nitrux 1.0.9 "NXOS" - Release amd64/' README.diskdefines
-rm md5sum.txt
+rm md5sum.txt && echo "REMOVED OLD md5sum.txt"
 
 find -type f -print0 | xargs -0 md5sum | grep -v isolinux/boot.cat | tee md5sum.txt
 
-xorriso -as mkisofs -r -V "Nitrux_Live" \
-	-J -l -b isolinux/isolinux.bin \
-	-c isolinux/boot.cat -no-emul-boot \
-	-isohybrid-mbr /usr/lib/syslinux/isohdpfx.bin \
-	-eltorito-alt-boot \
-	-isohybrid-gpt-basdat \
-	-boot-load-size 4 -boot-info-table \
+mkisofs -D -r -V "Nitrux_live" \
+	-cache-inodes -J -l \
+	-b isolinux/isolinux.bin \
+	-c isolinux/boot.cat \
+	-no-emul-boot \
+	-boot-load-size 4 \
+	-boot-info-table \
 	-o ../nitruxos.iso ./
+
+md5sum ../nitruxos.iso
