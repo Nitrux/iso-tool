8b5725d67282d6b53d4a293a1bbf73449eb36a29
luis-lavaire <llavaire01@gmail.com>
2018-02-12 09:30:29 -0600

    .

--- upload.sh
+++ upload.sh
@@ -5,9 +5,9 @@ mkdir release
 md5sum nitruxos.iso > release/md5.txt
 
 echo " ==> Uploading the ISO file..."
-curl --upload-file nitruxos.iso https://transfer.sh -H "Max-Days: 2" | tee release/URL
+curl --upload-file nitruxos.iso https://transfer.sh | tee release/URL
 
-wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh -O u.sh
+wget https://github.com/probonopd/uploadtool/raw/master/upload.sh -O u.sh
 
 echo " ==> Creating the release..."
 sh ./u.sh release/*
