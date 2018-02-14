6952ae8f55215fabdb3b57701d36e1857d663b35
luis-lavaire <llavaire01@gmail.com>
2018-02-13 18:32:52 -0600

    Just ignore messages, not errors during package installation.

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ apt-get install -qq -y busybox
 busybox wget -qO - http://repo.nxos.org/public.key | apt-key add -
 busybox wget -qO - http://origin.archive.neon.kde.org/public.key | apt-key add -
 apt-get -y update
-apt-get -y -qq install $PACKAGES > /dev/null 2>&1
+apt-get -y -qq install $PACKAGES > /dev/null
 apt-get -y clean
 useradd -m -U -G sudo,cdrom,adm,dip,plugdev me
 depmod -a $(uname -r)
