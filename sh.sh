c2aa0e46934df8cc486b8f6fecb222e2b8da6dd4
luis-lavaire <llavaire01@gmail.com>
2018-02-19 17:41:15 -0600

    Commit.

--- sh.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-debootstrap --verbose --components=main,restricted,universe,multiverse,stable \
-	--include=linux-image-generic \
-	--exclude=nano \
-	--arch amd64 xenial . http://us.archive.ubuntu.com/ubuntu/
