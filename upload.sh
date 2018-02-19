a9c01e0cffbc6c92dc7f2080a3716955f9628e0f
luis-lavaire <llavaire01@gmail.com>
2018-02-19 14:17:21 -0600

    Commit.

--- upload.sh
+++ upload.sh
@@ -5,8 +5,7 @@ mkdir release
 md5sum nitruxos.iso > release/md5.txt
 
 echo " ==> Uploading the ISO file..."
-curl --upload-file nitruxos.iso https://transfer.sh | tee release/URL
-echo ''
+curl --upload-file nitruxos.iso https://transfer.sh > release/URL
 
 wget https://github.com/probonopd/uploadtool/raw/master/upload.sh -O u.sh
 
