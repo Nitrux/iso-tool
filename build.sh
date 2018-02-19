7335e272839d9963fc66724dd9b736e384ffef9a
luis-lavaire <llavaire01@gmail.com>
2018-02-19 16:33:02 -0600

    Try this out.

--- build.sh
+++ build.sh
@@ -1,26 +1,16 @@
 #! /bin/sh
 
-# Prepare the workspace.
+# Prepare the root filesystem.
 
 mkdir -p \
 	filesystem \
 	iso/casper \
 	iso/boot/isolinux
 
-
-# Build the base filesystem.
-
-echo "Downloading base root filesystem."
 wget -q http://cdimage.ubuntu.com/ubuntu-base/releases/16.04.3/release/ubuntu-base-16.04.3-base-amd64.tar.gz -O base.tar.gz
 tar xf base.tar.gz -C filesystem/
 rm -rf filesystem/dev/*
 cp /etc/resolv.conf filesystem/etc/
-
-
-# Build the base filesystem.
-
-echo "Installing packages to root."
-PACKAGES="linux-image-generic nxos-desktop casper lupin-casper"
 
 mkdir -p \
 	filesystem/dev \
@@ -29,65 +19,50 @@ mkdir -p \
 mount -o bind /dev filesystem/dev || exit 1
 mount -o bind /proc filesystem/proc || exit 1
 
-#debootstrap --components=main \
-#	--include=linux-image-generic \
-#	--exclude=nano \
-#	--arch amd64 xenial filesystem/ http://us.archive.ubuntu.com/ubuntu/
 
+# Install the nxos-desktop to `filesystem/`
+
+PACKAGES="initramfs-tools linux-image-generic nxos-desktop casper lupin-casper"
 chroot filesystem/ sh -c "
-export LANG=C
-export LC_ALL=C
-
-apt-get update
-apt-get install -y apt-transport-https wget ca-certificates
-
-sed -i 's/#.*$//;/^$/d' /etc/apt/sources.list
-
-wget -q https://archive.neon.kde.org/public.key -O neon.key
-if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key | sha256sum -c; then
-	apt-key add neon.key
-	echo deb http://archive.neon.kde.org/dev/stable xenial main >> /etc/apt/sources.list
-	echo deb http://archive.neon.kde.org/user xenial main >> /etc/apt/sources.list
-fi
-rm neon.key
-
-wget -q http://repo.nxos.org/public.key -O nxos.key
-if echo de7501e2951a9178173f67bdd29a9de45a572f19e387db5f4e29eb22100c2d0e nxos.key | sha256sum -c; then
-	apt-key add nxos.key
-	echo deb http://repo.nxos.org nxos main >> /etc/apt/sources.list
-	echo deb http://repo.nxos.org xenial main >> /etc/apt/sources.list
-fi
-rm nxos.key
-
-set -x
-apt-get update
-apt-get -qq install $PACKAGES
-apt-get clean
-useradd -m -U -G sudo,cdrom,adm,dip,plugdev me
-find /var/log -regex '.*?[0-9].*?' -exec rm -v {} \;
-rm /etc/resolv.conf
-umount /dev
-umount /proc
-"
-
-
-
-# Add the kernel and the initramfs to the ISO.
-
-echo "====================="
-ls filesystem/boot
-echo "====================="
-exit 1
+	export LANG=C
+	export LC_ALL=C
+	
+	apt-get update
+	apt-get install -y apt-transport-https wget ca-certificates
+	
+	sed -i 's/#.*$//;/^$/d' /etc/apt/sources.list
+	
+	wget -q https://archive.neon.kde.org/public.key -O neon.key
+	if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key | sha256sum -c; then
+		apt-key add neon.key
+		echo deb http://archive.neon.kde.org/dev/stable xenial main >> /etc/apt/sources.list
+		echo deb http://archive.neon.kde.org/user xenial main >> /etc/apt/sources.list
+	fi
+	rm neon.key
+	
+	wget -q http://repo.nxos.org/public.key -O nxos.key
+	if echo de7501e2951a9178173f67bdd29a9de45a572f19e387db5f4e29eb22100c2d0e nxos.key | sha256sum -c; then
+		apt-key add nxos.key
+		echo deb http://repo.nxos.org nxos main >> /etc/apt/sources.list
+		echo deb http://repo.nxos.org xenial main >> /etc/apt/sources.list
+	fi
+	rm nxos.key
+	
+	apt-get update
+	apt-get -qq install $PACKAGES > /dev/null
+	apt-get clean
+	useradd -m -U -G sudo,cdrom,adm,dip,plugdev me
+	find /var/log -regex '.*?[0-9].*?' -exec rm -v {} \;
+	rm /etc/resolv.conf
+	umount /dev
+	umount /proc
+	"
 
 cp filesystem/vmlinuz iso/boot/linux
 cp filesystem/initrd.img iso/boot/initramfs
 
-(sleep 300; echo ' • ') &
-echo "Compressing the new filesystem"
-mksquashfs filesystem/ iso/casper/filesystem.squashfs -comp xz -no-progress -b 1M
 
-
-# Clean the root directory before compressing and packing it.
+# Clean the filesystem.
 
 rm -rf filesystem/tmp/* \
 	filesystem/boot/* \
@@ -96,7 +71,12 @@ rm -rf filesystem/tmp/* \
 	filesystem/var/log/* \
 	filesystem/var/lib/dbus/machine-id
 
-# Create the ISO file.
+
+# Compress the root filesystem and create the ISO.
+
+(sleep 300; echo ' • ') &
+echo "Compressing the root filesystem"
+mksquashfs filesystem/ iso/casper/filesystem.squashfs -comp xz -no-progress -b 1M
 
 wget -q http://kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.tar.xz -O syslinux.tar.xz
 tar xf syslinux.tar.xz
@@ -109,8 +89,7 @@ echo "default /boot/linux initrd=/boot/initramfs BOOT=casper quiet splash" > boo
 echo -n $(du -sx --block-size=1 . | tail -1 | awk '{ print $1 }') > casper/filesystem.size
 find -type f -print0 | xargs -0 md5sum | grep -v isolinux/boot.cat > md5sum.txt
 
-
-# TODO: support UEFI by default.
+# TODO: support for UEFI boot.
 xorriso -as mkisofs -V "NXOS" \
 	-J -l -D -r \
 	-no-emul-boot \
