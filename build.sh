d37525ad30ae174d1f535a7b27b4bfbed06ff251
luis-lavaire <llavaire01@gmail.com>
2018-02-20 14:29:01 -0600

    Commit.

--- build.sh
+++ build.sh
@@ -48,9 +48,8 @@ chroot filesystem/ sh -c "
 	fi
 	rm nxos.key
 
-	exec > /dev/null
 	apt-get update
-	apt-get -qq install $PACKAGES
+	apt-get -qq install $PACKAGES > /dev/null
 	apt-get clean
 	useradd -m -U -G sudo,cdrom,adm,dip,plugdev me
 	find /var/log -regex '.*?[0-9].*?' -exec rm -v {} \;
