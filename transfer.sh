dfd3782d3b6d2cc99c98729885ef04c86170268a
luis-lavaire <llavaire01@gmail.com>
2018-02-28 21:11:33 -0600

    Fixed bad syntax when executing the upload script.

--- transfer.sh
+++ transfer.sh
@@ -4,4 +4,4 @@ sha256sum nxos.iso > checksum
 curl -i -F filedata=@checksum -F filedata=@nxos.iso https://transfer.sh | sed 's/http/\nhttp/g' | grep http > urls
 
 wget -q https://github.com/probonopd/uploadtool/raw/master/upload.sh
-sh -c ./upload.sh urls
+sh ./upload.sh urls
