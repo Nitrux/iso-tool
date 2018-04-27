2bf6ff2ba488c83cc2e3e582f728a7f099ce4eea
Luis Lavaire <llavaire01@gmail.com>
2018-04-26 22:27:42 -0600

    Fix /dev issue. Updated download links.

--- build.sh
+++ build.sh
@@ -7,52 +7,53 @@ mkdir -p \
 	iso/casper \
 	iso/boot/isolinux
 
-wget -q http://cdimage.ubuntu.com/ubuntu-base/daily/20180409/bionic-base-amd64.tar.gz
-tar xf *.tar.gz -C filesystem
+wget -q http://cdimage.ubuntu.com/ubuntu-base/daily/current/bionic-base-amd64.tar.gz
+tar xf *.tar.gz -C filesystem/
 
-rm -rf filesystem/dev/*
+
+# Run a command in a chroot safely.
+
+FS_DIR=filesystem/
+
+rm -rf $FS_DIR/dev/*
 cp /etc/resolv.conf filesystem/etc/
 
-mkdir -p \
-	filesystem/dev \
-	filesystem/proc
-
-mount -t proc none filesystem/proc || exit 1
-mount -t devtmpfs none filesystem/dev || exit 1
+mount -t proc -o nodev none $FS_DIR/proc || exit 1
+mount -t devtmpfs -o nodev none $FS_DIR/dev || exit 1
 
 mkdir -p /dev/pts
-mount -t devpts none filesystem/dev/pts || exit 1
+mount -t devpts -o nodev none $FS_DIR/dev/pts || exit 1
 
+cp ./config/config.sh $FS_DIR/
+chroot $FS_DIR/ sh -c "/config.sh"
+rm -r $FS_DIR/config.sh
 
-# Install the nxos-desktop to `filesystem/`
+umount -f $FS_DIR/dev/pts
+umount -f $FS_DIR/dev
+umount -f $FS_DIR/proc
 
-cp config/chroot.sh filesystem/
-chroot filesystem/ /bin/sh /chroot.sh
-rm -r filesystem/chroot.sh
-
-umount filesystem/dev
-umount filesystem/proc
-
-cp filesystem/vmlinuz iso/boot/linux
-cp filesystem/initrd.img iso/boot/initramfs
+cp $FS_DIR/vmlinuz iso/boot/kernel
+cp $FS_DIR/initrd.img iso/boot/initramfs
 
 
 # Clean the filesystem.
 
-rm -rf filesystem/tmp/* \
-	filesystem/boot/* \
-	filesystem/vmlinuz* \
-	filesystem/initrd.img* \
-	filesystem/var/log/* \
-	filesystem/var/lib/dbus/machine-id
+rm -rf $FS_DIR/tmp/* \
+	$FS_DIR/boot \
+	$FS_DIR/vmlinuz* \
+	$FS_DIR/initrd.img* \
+	$FS_DIR/var/log/* \
+	$FS_DIR/var/lib/dbus/machine-id
 
 
-# Compress the root filesystem and create the ISO.
+# Compress the root filesystem.
 
 (sleep 300; echo +) &
 echo "Compressing the root filesystem"
-mksquashfs filesystem/ iso/casper/filesystem.squashfs -comp xz -no-progress
+mksquashfs $FS_DIR/ iso/casper/filesystem.squashfs -comp xz -no-progress
 
+
+# Download SYSLINUX.
 
 wget -q -nc https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.tar.xz
 tar xf syslinux-6.03.tar.xz
@@ -67,10 +68,13 @@ cp $SL/bios/core/isolinux.bin \
 	$SL/bios/com32/elflink/ldlinux/ldlinux.c32 \
 	iso/boot/isolinux/
 
+
+# Prepare the ISO files.
+
 cd iso/
 
-wget -q -nc https://raw.githubusercontent.com/nomad-desktop/isolinux-nomad-theme/master/splash.png -O boot/isolinux/splash.png
-wget -q -nc https://raw.githubusercontent.com/nomad-desktop/isolinux-nomad-theme/master/theme.txt -O boot/isolinux/theme.txt
+wget -q https://raw.githubusercontent.com/nomad-desktop/isolinux-theme-nomad/master/splash.png -O boot/isolinux/splash.png
+wget -q https://raw.githubusercontent.com/nomad-desktop/isolinux-theme-nomad/master/theme.txt -O boot/isolinux/theme.txt
 
 echo '
 default vesamenu.c32
@@ -78,19 +82,21 @@ include theme.txt
 
 menu title Installer boot menu.
 label Try Nitrux
-	kernel /boot/linux
+	kernel /boot/kernel
 	append initrd=/boot/initramfs boot=casper elevator=noop quiet splash
 
 label Try Nitrux (safe graphics mode)
-	kernel /boot/linux
+	kernel /boot/kernel
 	append initrd=/boot/initramfs boot=casper nomodeset elevator=noop quiet splash
 
 menu tabmsg Press ENTER to boot or TAB to edit a menu entry
 ' > boot/isolinux/syslinux.cfg
 
-echo -n $(du -sx --block-size=1 . | tail -1 | awk '{ print $1 }') > casper/filesystem.size
+echo -n $(du -sx --block-size=1 . | tail -n 1 | awk '{ print $1 }') > casper/filesystem.size
+
 
 # TODO: create UEFI images.
+# Create the ISO image.
 
 xorriso -as mkisofs \
 	-o ../nxos.iso \
@@ -101,3 +107,5 @@ xorriso -as mkisofs \
 	-b boot/isolinux/isolinux.bin \
 	-isohybrid-mbr boot/isolinux/isohdpfx.bin \
 	./
+
+echo "zsync|http://server.domain/path/your.iso.zsync" | dd of=../nxos.iso bs=1 seek=33651 count=512 conv=notrunc
--- config/config.sh
+++ config/config.sh
@@ -12,21 +12,19 @@ casper
 PACKAGES=$(echo $PACKAGES | tr '\n' ' ')
 
 apt-get update
-apt-get install -y apt-transport-https wget ca-certificates gnupg2
+apt-get install -y apt-transport-https wget ca-certificates
 
 wget -q https://archive.neon.kde.org/public.key -O neon.key
 if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key | sha256sum -c; then
 	apt-key add neon.key
-	echo deb http://archive.neon.kde.org/user xenial main >> /etc/apt/sources.list
-	echo deb http://archive.neon.kde.org/dev/stable xenial main >> /etc/apt/sources.list
-	echo deb http://archive.neon.kde.org/dev/unstable xenial main >> /etc/apt/sources.list
+	echo 'deb http://archive.neon.kde.org/dev/unstable/ bionic main' > /etc/apt/sources.list
 fi
 rm neon.key
 
 wget -q http://repo.nxos.org/public.key -O nxos.key
 if echo b51f77c43f28b48b14a4e06479c01afba4e54c37dc6eb6ae7f51c5751929fccc nxos.key | sha256sum -c; then
 	apt-key add nxos.key
-	echo deb http://repo.nxos.org/stable nxos main >> /etc/apt/sources.list
+	echo 'deb http://repo.nxos.org/testing/ nxos main' > /etc/apt/sources.list
 fi
 rm nxos.key
 
