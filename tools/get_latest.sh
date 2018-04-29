a27c433b77a881fe37c5ab3932db4c6356883414
Luis Lavaire <llavaire01@gmail.com>
2018-04-28 23:46:41 -0600

    [ci skip] Repository was moved to another account.

--- tools/get_latest.sh
+++ tools/get_latest.sh
@@ -4,7 +4,7 @@ mkdir latest
 cd latest
 
 echo " ==> Fetching latest release..."
-for url in $(wget -qO - https://github.com/luis-lavaire/mkiso/releases/download/continuous/urls); do
+for url in $(wget -qO - https://github.com/nomad-desktop/mkiso/releases/download/continuous/urls); do
 	wget -q -nc --show-progress $url
 done
 
