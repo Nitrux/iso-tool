bba9b3f6c37f61c5d3d7db9e6edb0f92ef9a112c
UriHerrera <hello@uriherrera.com>
2018-10-01 19:13:54 -0500

    updated mkiso scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,57 +9,17 @@ localechooser-data
 cifs-utils
 casper
 lupin-casper
-binutils-multiarch
 iputils-ping
 dhcpcd5
-nomad-desktop
 apt-utils
-sudo
 '
-apt-get -qq update
-apt-get -qq install -y apt-transport-https wget ca-certificates gnupg2
+apt update
+apt install -y apt-transport-https wget ca-certificates gnupg2
 
 # Use optimized sources.list. This sources.list includes the current Ubuntu development release as the main repository and also includes the latest LTS release.
 # The LTS repositories are included to add support for the KDE Neon repository since these packages are built against this release of Ubuntu.
 
 rm /etc/apt/sources.list
-echo '################' >> /etc/apt/sources.list
-echo '# Ubuntu Repos #' >> /etc/apt/sources.list
-echo '################' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '### Main' >> /etc/apt/sources.list
-echo '# deb http://archive.ubuntu.com/ubuntu cosmic main restricted universe multiverse' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '### Updates' >> /etc/apt/sources.list
-echo '# deb http://archive.ubuntu.com/ubuntu cosmic-security main restricted universe multiverse' >> /etc/apt/sources.list
-echo '# deb http://archive.ubuntu.com/ubuntu cosmic-updates main restricted universe multiverse' >> /etc/apt/sources.list
-echo '# deb http://archive.ubuntu.com/ubuntu cosmic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
-echo '# deb http://archive.ubuntu.com/ubuntu cosmic-backports main restcited universe multiverse' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '### Partner' >> /etc/apt/sources.list
-echo '# deb http://archive.ubuntu.com/ubuntu cosmic partner' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '#################' >> /etc/apt/sources.list
-echo '# Ubuntu Source #' >> /etc/apt/sources.list
-echo '#################' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '### Main' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic main restricted universe multiverse' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '### Updates' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-security main restricted universe multiverse' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-updates main restricted universe multiverse' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-backports main restcited universe multiverse' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '#####################################################################################' >> /etc/apt/sources.list
-echo '# The Bionic repositories are included to provide support for KDE Neon repositories #' >> /etc/apt/sources.list
-echo '# Since there are times when updates request packages that were removed in newer    #' >> /etc/apt/sources.list
-echo '# versions of Ubuntu resulting in APT holding back the upgrades. This allows the    #' >> /etc/apt/sources.list
-echo '# installation of these packages.                                                   #' >> /etc/apt/sources.list
-echo '#####################################################################################' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
 echo '#######################' >> /etc/apt/sources.list
 echo '# Ubuntu Repos Bionic #' >> /etc/apt/sources.list
 echo '#######################' >> /etc/apt/sources.list
@@ -76,19 +36,8 @@ echo '' >> /etc/apt/sources.list
 echo '### Partner' >> /etc/apt/sources.list
 echo '# deb http://archive.ubuntu.com/ubuntu bionic partner' >> /etc/apt/sources.list
 echo '' >> /etc/apt/sources.list
-echo '########################' >> /etc/apt/sources.list
-echo '# Ubuntu Source Bionic #' >> /etc/apt/sources.list
-echo '########################' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '### Main' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
-echo '### Updates' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-security main restricted universe multiverse' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
-echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-backports main restcited universe multiverse' >> /etc/apt/sources.list
-echo '' >> /etc/apt/sources.list
+
+apt update
 
 wget -q https://archive.neon.kde.org/public.key -O neon.key
 if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key | sha256sum -c; then
@@ -104,6 +53,8 @@ if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.ke
 fi
 rm neon.key
 
+apt update
+
 wget -q http://repo.nxos.org/public.key -O nxos.key
 if echo b51f77c43f28b48b14a4e06479c01afba4e54c37dc6eb6ae7f51c5751929fccc nxos.key | sha256sum -c; then
 	apt-key add nxos.key
@@ -117,10 +68,10 @@ if echo b51f77c43f28b48b14a4e06479c01afba4e54c37dc6eb6ae7f51c5751929fccc nxos.ke
 fi
 rm nxos.key
 
-apt-get -qq update
-apt-get -qq install -f
-apt-get -qq install -y $(echo $PACKAGES | tr '\n' ' ') > /dev/null
-apt-get clean
+apt update
+apt install -y nomad-desktop
+apt install -y $(echo $PACKAGES | tr '\n' ' ') > /dev/null
+apt clean
 
 
 # Install AppImages.
