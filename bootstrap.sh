17f8590a8942a20ca03a004a07881f1db1a60a88
UriHerrera <hello@uriherrera.com>
2018-10-01 19:03:48 -0500

    updated mkiso scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,14 +14,52 @@ iputils-ping
 dhcpcd5
 nomad-desktop
 apt-utils
+sudo
 '
 apt-get -qq update
-apt-get -qq install -y apt-transport-https wget ca-certificates gnupg2 sudo libelf1 kmod linux-base
+apt-get -qq install -y apt-transport-https wget ca-certificates gnupg2
 
 # Use optimized sources.list. This sources.list includes the current Ubuntu development release as the main repository and also includes the latest LTS release.
 # The LTS repositories are included to add support for the KDE Neon repository since these packages are built against this release of Ubuntu.
 
 rm /etc/apt/sources.list
+echo '################' >> /etc/apt/sources.list
+echo '# Ubuntu Repos #' >> /etc/apt/sources.list
+echo '################' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Main' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu cosmic main restricted universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Updates' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu cosmic-security main restricted universe multiverse' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu cosmic-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu cosmic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu cosmic-backports main restcited universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Partner' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu cosmic partner' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '#################' >> /etc/apt/sources.list
+echo '# Ubuntu Source #' >> /etc/apt/sources.list
+echo '#################' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Main' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic main restricted universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Updates' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-security main restricted universe multiverse' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu cosmic-backports main restcited universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '#####################################################################################' >> /etc/apt/sources.list
+echo '# The Bionic repositories are included to provide support for KDE Neon repositories #' >> /etc/apt/sources.list
+echo '# Since there are times when updates request packages that were removed in newer    #' >> /etc/apt/sources.list
+echo '# versions of Ubuntu resulting in APT holding back the upgrades. This allows the    #' >> /etc/apt/sources.list
+echo '# installation of these packages.                                                   #' >> /etc/apt/sources.list
+echo '#####################################################################################' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
 echo '#######################' >> /etc/apt/sources.list
 echo '# Ubuntu Repos Bionic #' >> /etc/apt/sources.list
 echo '#######################' >> /etc/apt/sources.list
@@ -38,8 +76,19 @@ echo '' >> /etc/apt/sources.list
 echo '### Partner' >> /etc/apt/sources.list
 echo '# deb http://archive.ubuntu.com/ubuntu bionic partner' >> /etc/apt/sources.list
 echo '' >> /etc/apt/sources.list
-
-apt-get update
+echo '########################' >> /etc/apt/sources.list
+echo '# Ubuntu Source Bionic #' >> /etc/apt/sources.list
+echo '########################' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Main' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Updates' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-security main restricted universe multiverse' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
+echo '# deb-src http://archive.ubuntu.com/ubuntu bionic-backports main restcited universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
 
 wget -q https://archive.neon.kde.org/public.key -O neon.key
 if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key | sha256sum -c; then
@@ -63,14 +112,16 @@ if echo b51f77c43f28b48b14a4e06479c01afba4e54c37dc6eb6ae7f51c5751929fccc nxos.ke
 	echo '################' >> /etc/apt/sources.list
 	echo 'deb http://repo.nxos.org/stable/ nxos main' >> /etc/apt/sources.list
 	echo 'deb http://repo.nxos.org/development/ nxos main' >> /etc/apt/sources.list
-	echo '# deb http://repo.nxos.org/testing/ nxos main' >> /etc/apt/sources.list
+	echo 'deb http://repo.nxos.org/testing/ nxos main' >> /etc/apt/sources.list
 	echo '' >> /etc/apt/sources.list
 fi
 rm nxos.key
 
-apt-get update
-apt-get install -y $(echo $PACKAGES | tr '\n' ' ')
+apt-get -qq update
+apt-get -qq install -f
+apt-get -qq install -y $(echo $PACKAGES | tr '\n' ' ') > /dev/null
 apt-get clean
+
 
 # Install AppImages.
 
@@ -108,7 +159,7 @@ wget -q https://raw.githubusercontent.com/luis-lavaire/znx-gui/master/znx-gui -O
 chmod +x /bin/znx-gui
 
 
-# Install the latest stable kernel.d5
+# Install the latest stable kernel
 
 kfiles='
 http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.11/linux-headers-4.18.11-041811_4.18.11-041811.201809290731_all.deb
