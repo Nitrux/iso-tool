34ee3c73ffb85169d4a11c3557747d8d9e1843be
UriHerrera <hello@uriherrera.com>
2018-10-01 19:32:38 -0500

    updated mkiso scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,44 +11,44 @@ casper
 lupin-casper
 iputils-ping
 dhcpcd5
-apt-utils
 '
 apt update
-apt install -y apt-transport-https wget ca-certificates gnupg2
+apt install -y apt-transport-https wget ca-certificates gnupg2 apt-utils
 
 # Use optimized sources.list. This sources.list includes the current Ubuntu development release as the main repository and also includes the latest LTS release.
 # The LTS repositories are included to add support for the KDE Neon repository since these packages are built against this release of Ubuntu.
 
-# rm /etc/apt/sources.list
-# echo '#######################' >> /etc/apt/sources.list
-# echo '# Ubuntu Repos Bionic #' >> /etc/apt/sources.list
-# echo '#######################' >> /etc/apt/sources.list
-# echo '' >> /etc/apt/sources.list
-# echo '### Main' >> /etc/apt/sources.list
-# echo 'deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse' >> /etc/apt/sources.list
-# echo '' >> /etc/apt/sources.list
-# echo '### Updates' >> /etc/apt/sources.list
-# echo 'deb http://archive.ubuntu.com/ubuntu bionic-security main restricted universe multiverse' >> /etc/apt/sources.list
-# echo 'deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse' >> /etc/apt/sources.list
-# echo '# deb http://archive.ubuntu.com/ubuntu bionic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
-# echo '# deb http://archive.ubuntu.com/ubuntu bionic-backports main restcited universe multiverse' >> /etc/apt/sources.list
-# echo '' >> /etc/apt/sources.list
-# echo '### Partner' >> /etc/apt/sources.list
-# echo '# deb http://archive.ubuntu.com/ubuntu bionic partner' >> /etc/apt/sources.list
-#'
+rm /etc/apt/sources.list
+echo '#######################' >> /etc/apt/sources.list
+echo '# Ubuntu Repos Bionic #' >> /etc/apt/sources.list
+echo '#######################' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Main' >> /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Updates' >> /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu bionic-security main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu bionic-proposed main restcited universe multiverse' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu bionic-backports main restcited universe multiverse' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+echo '### Partner' >> /etc/apt/sources.list
+echo '# deb http://archive.ubuntu.com/ubuntu bionic partner' >> /etc/apt/sources.list
+echo '' >> /etc/apt/sources.list
+
 apt update
 
 wget -q https://archive.neon.kde.org/public.key -O neon.key
 if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key | sha256sum -c; then
 	apt-key add neon.key
-	echo '##################' >> /etc/apt/sources.list.d/neon.list
-	echo '# KDE Neon Repos #' >> /etc/apt/sources.list.d/neon.list
-	echo '##################' >> /etc/apt/sources.list.d/neon.list
-	echo '' >> /etc/apt/sources.list.d/neon.list
-	echo '# deb http://archive.neon.kde.org/dev/unstable/ bionic main' >> /etc/apt/sources.list.d/neon.list
-	echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main' >> /etc/apt/sources.list.d/neon.list
-	echo '# deb http://archive.neon.kde.org/user bionic main' >> /etc/apt/sources.list.d/neon.list
-	echo '' >> /etc/apt/sources.list.d/neon.list
+	echo '##################' >> /etc/apt/sources.list
+	echo '# KDE Neon Repos #' >> /etc/apt/sources.list
+	echo '##################' >> /etc/apt/sources.list
+	echo '' >> /etc/apt/sources.list
+	echo '# deb http://archive.neon.kde.org/dev/unstable/ bionic main' >> /etc/apt/sources.list
+	echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main' >> /etc/apt/sources.list
+	echo '# deb http://archive.neon.kde.org/user bionic main' >> /etc/apt/sources.list
+	echo '' >> /etc/apt/sources.list
 fi
 rm neon.key
 
@@ -57,13 +57,13 @@ apt update
 wget -q http://repo.nxos.org/public.key -O nxos.key
 if echo b51f77c43f28b48b14a4e06479c01afba4e54c37dc6eb6ae7f51c5751929fccc nxos.key | sha256sum -c; then
 	apt-key add nxos.key
-	echo '################' >> /etc/apt/sources.list.d/nxos.list
-	echo '# Nitrux Repos #' >> /etc/apt/sources.list.d/nxos.list
-	echo '################' >> /etc/apt/sources.list.d/nxos.list
-	echo 'deb http://repo.nxos.org/stable/ nxos main' >> /etc/apt/sources.list.d/nxos.list
-	echo 'deb http://repo.nxos.org/development/ nxos main' >> /etc/apt/sources.list.d/nxos.list
-	echo 'deb http://repo.nxos.org/testing/ nxos main' >> /etc/apt/sources.list.d/nxos.list
-	echo '' >> /etc/apt/sources.list.d/nxos.list
+	echo '################' >> /etc/apt/sources.list
+	echo '# Nitrux Repos #' >> /etc/apt/sources.list
+	echo '################' >> /etc/apt/sources.list
+	echo 'deb http://repo.nxos.org/stable/ nxos main' >> /etc/apt/sources.list
+	echo 'deb http://repo.nxos.org/development/ nxos main' >> /etc/apt/sources.list
+	echo 'deb http://repo.nxos.org/testing/ nxos main' >> /etc/apt/sources.list
+	echo '' >> /etc/apt/sources.list
 fi
 rm nxos.key
 
