2b6fd03932bcf1490c10e22dbc6446361922927d
UriHerrera <hello@uriherrera.com>
2018-10-13 01:43:01 -0500

    updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,6 @@ localechooser-data
 cifs-utils
 casper
 lupin-casper
-nomad-desktop
 '
 
 
@@ -67,8 +66,8 @@ rm nxos.key
 # -- avoiding recommended packages.
 
 apt -qq update
-apt -yy -qq upgrade
-apt -yy -qq install $(echo $PACKAGES | tr '\n' ' ') --no-install-recommends > /dev/null
+apt -yy -qq install $(echo $PACKAGES | tr '\n' ' ') --no-install-recommends > /dev/null\
+apt -yy -qq install nomad-desktop > /dev/null
 apt -yy -qq install --only-upgrade base-files=10.4+nxos > /dev/null
 apt -qq clean
 apt -qq autoclean
