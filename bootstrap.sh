b0d88a1a431a8874280362fb64314e5643b0ae6e
UriHerrera <hello@uriherrera.com>
2018-10-16 17:16:16 -0500

    keep appimages in /Applications

--- bootstrap.sh
+++ bootstrap.sh
@@ -202,4 +202,4 @@ mkdir /etc/skel/Applications
 
 # -- Move AppImages to the user /Applications dir.
 
-mv /Applications/*.AppImage /etc/skel/Applications
+#   mv /Applications/*.AppImage /etc/skel/Applications
