4afac096397bcbadc766f45cc6067709fab29546
UriHerrera <hello@uriherrera.com>
2018-10-24 02:39:29 -0500

    change permissions of appimages to 744

--- bootstrap.sh
+++ bootstrap.sh
@@ -65,7 +65,7 @@ for x in $(echo $APPS | tr '\n' ' '); do
 	wget -qP /Applications $x
 done
 
-chmod 644 /Applications/*
+chmod 744 /Applications/*
 
 
 # -- Create /Applications dir for users. This dir "should" be created by the Software Center.
