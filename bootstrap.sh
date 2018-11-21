86ddc24c347696564fc1a4c240a2afe2ea1595a9
UriHerrera <hello@uriherrera.com>
2018-11-20 19:59:39 -0600

    xxx

--- bootstrap.sh
+++ bootstrap.sh
@@ -192,3 +192,23 @@ update-initramfs -u
 # -- Fix for https://bugs.launchpad.net/ubuntu/+source/network-manager/+bug/1638842.
 
 cp /configs/10-globally-managed-devices.conf /etc/NetworkManager/conf.d/
+
+# -- Add Steam
+
+apt -yy install curl gstreamer1.0-gl libbrotli1 libgraphene-1.0-0 libgstreamer-gl1.0-0 libjavascriptcoregtk-4.0-18 libwebkitgtk-4.0-37 libwoff1 python-apt zenity zenity-common > /dev/null --no-install-recommends
+
+apt -yy install gcc-8-base:i386 libbsd0:i386 libc6:i386 libdrm-amdgpu1:i386 libdrm-intel1:i386 libdrm-nouveau2:i386 libdrm-radeon1:i386 libdrm2:i386 libedit2:i386 libelf1:i386 libexpat1:i386 libffi6:i386 libgcc1:i386 libgl1:i386 libgl1-mesa-dri:i386 libgl1-mesa-glx:i386 libglapi-mesa:i386 libglvnd0:i386 libglx-mesa0:i386 libglx0:i386 libllvm7:i386 libpciaccess0:i386 libsensors4:i386 libstdc++6:i386 libtinfo5:i386 libx11-6:i386 libx11-xcb1:i386 libxau6:i386 libxcb-dri2-0:i386 libxcb-dri3-0:i386 libxcb-glx0:i386 libxcb-present0:i386 libxcb-sync1:i386 libxcb1:i386 libxdamage1:i386 libxdmcp6:i386 libxext6:i386 libxfixes3:i386 libxshmfence1:i386 libxxf86vm1:i386 xbitmaps xterm zlib1g:i386 > /dev/null --no-install-recommends
+
+steam='
+http://media.steampowered.com/client/installer/steam.deb
+'
+
+mkdir steam_deb
+
+for x in steam; do
+	wget -q -P steam_deb $x
+done
+
+dpkg -iR steam_deb > /dev/null
+rm -r steam_deb
+
