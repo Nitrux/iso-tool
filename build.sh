517902b45d59ee430db7b03ad2bca13ce1ca2067
Luis Lavaire <llavaire01@gmail.com>
2018-12-12 01:21:42 -0600

    Commit.

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ tar xf base.tar.gz -C $BUILD_DIR
 wget -qO /bin/runc https://raw.githubusercontent.com/Nitrux/runc/master/runc
 chmod +x /bin/runc
 
-bash -x /bin/runc -d configs $BUILD_DIR bootstrap.sh || true
+runc -d configs $BUILD_DIR bootstrap.sh || true
 
 
 # -- Copy the kernel and initramfs to $ISO_DIR.
