7e58c87bd136e1ebd536b749e0b32400d354f5a8
Uri Herrera <hello@uriherrera.com>
2019-01-29 13:08:48 -0600

    see output from installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,7 +52,7 @@ cp /configs/sources.list /etc/apt/sources.list
 
 apt -qq update > /dev/null
 apt -yy -qq upgrade > /dev/null
-apt -yy -qq install ${PACKAGES//\\n/ } --no-install-recommends > /dev/null
+apt -yy -qq install ${PACKAGES//\\n/ } --no-install-recommends
 
 
 # -- Add AppImages.
