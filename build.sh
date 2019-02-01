203ecaf33bc5478e98a81ef421f41d1a495ee731
Luis Lavaire <llavaire01@gmail.com>
2019-02-01 17:55:32 -0600

    Use 1MB blocks in the squashfs file.

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ cp $BUILD_DIR/initrd.img $ISO_DIR/boot/initramfs
 (while :; do sleep 300; printf "."; done) &
 
 mkdir -p $ISO_DIR/casper
-mksquashfs $BUILD_DIR $ISO_DIR/casper/filesystem.squashfs -comp xz -no-progress
+mksquashfs $BUILD_DIR $ISO_DIR/casper/filesystem.squashfs -comp xz -no-progress -b 1M
 
 
 # -- Write the commit hash that generated the image.
