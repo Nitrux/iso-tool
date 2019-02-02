6d1eb2b206b38dc295b783b1701a16208d54bfa7
Uri Herrera <hello@uriherrera.com>
2019-02-02 01:11:34 -0600

    update kernel links

--- bootstrap.sh
+++ bootstrap.sh
@@ -211,8 +211,8 @@ printf "INSTALLING NEW KERNEL."
 
 kfiles='
 https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20.6/linux-headers-4.20.6-042006_4.20.6-042006.201901310331_all.deb
-https://gitlab.com/Queuecumber/linux-acs-override/-/jobs/151541520/artifacts/raw/linux-headers-4.20.5-acso_4.20.5-acso-1_amd64.deb
-https://gitlab.com/Queuecumber/linux-acs-override/-/jobs/151541520/artifacts/raw/linux-image-4.20.5-acso_4.20.5-acso-1_amd64.deb
+https://gitlab.com/Queuecumber/linux-acs-override/-/jobs/153999927/artifacts/raw/linux-headers-4.20.6-acso_4.20.6-acso-1_amd64.deb
+https://gitlab.com/Queuecumber/linux-acs-override/-/jobs/153999927/artifacts/raw/linux-image-4.20.6-acso_4.20.6-acso-1_amd64.deb
 https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20.6/linux-modules-4.20.6-042006-generic_4.20.6-042006.201901310331_amd64.deb
 '
 
