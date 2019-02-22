542f3ee71be3a4a955ea2533c8ef43f55a28dfeb
UriHerrera <hello@nxos.org>
2019-02-22 17:20:42 -0600

    use stable znx

--- bootstrap.sh
+++ bootstrap.sh
@@ -66,7 +66,7 @@ sed -i "/env_reset/d" /etc/sudoers
 # -- Rename AppImageUpdate and znx.
 
 APPS_SYS='
-https://github.com/Nitrux/znx/releases/download/continuous-development/znx_development
+https://github.com/Nitrux/znx/releases/download/continuous-stable/znx_stable
 https://github.com/AppImage/AppImageUpdate/releases/download/continuous/AppImageUpdate-x86_64.AppImage
 '
 
@@ -92,7 +92,7 @@ done
 chmod +x /etc/skel/Applications/*
 
 mv /Applications/AppImageUpdate-x86_64.AppImage /Applications/AppImageUpdate
-mv /Applications/znx_development /Applications/znx
+mv /Applications/znx_stable /Applications/znx
 
 # -- Add znx-gui.
 
