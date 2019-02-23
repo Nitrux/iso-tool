751af3507449ab99f6d810307cf21284f8afec28
UriHerrera <hello@nxos.org>
2019-02-22 19:49:01 -0600

    show output

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,7 +49,7 @@ cp /configs/sources.list /etc/apt/sources.list
 
 apt -qq update > /dev/null
 apt -yy -qq upgrade > /dev/null
-apt -yy -qq install ${PACKAGES//\\n/ } --no-install-recommends > /dev/null
+apt -yy -qq install ${PACKAGES//\\n/ } --no-install-recommends
 apt -yy -qq purge --remove vlc > /dev/null
 
 
