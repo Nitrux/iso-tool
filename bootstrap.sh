5d12dd39be098a4f2189356267c6e27f2a47ee7b
UriHerrera <hello@nxos.org>
2019-03-26 19:18:28 -0600

    check apt output

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,10 +50,10 @@ cp /configs/sources.list /etc/apt/sources.list
 # -- Update packages list and install packages. Install Nomad Desktop meta package and base-files package
 # -- avoiding recommended packages.
 
-apt -qq update > /dev/null
-apt -yy -qq upgrade > /dev/null
-apt -yy -qq install ${PACKAGES//\\n/ } --no-install-recommends > /dev/null
-apt -yy -qq purge --remove vlc > /dev/null
+apt update
+apt -yy -qq upgrade
+apt -yy -qq install ${PACKAGES//\\n/ } --no-install-recommends
+apt -yy -qq purge --remove vlc
 
 
 # -- Add /Applications to $PATH.
