2832ba90ce225110b0a5f88ae1556f0f780842bf
Uri Herrera <hello@uriherrera.com>
2019-04-24 18:07:33 -0500

    tryout

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,16 +29,16 @@ apt -yy -qq install apt-transport-https wget ca-certificates gnupg2 apt-utils --
 # -- Add key for the Graphics Driver PPA.
 # -- Add key for the Ubuntu-X PPA.
 
-# 	wget -q https://archive.neon.kde.org/public.key -O neon.key
-# 	printf "ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key" | sha256sum -c &&
-# 	apt-key add neon.key
+	wget -q https://archive.neon.kde.org/public.key -O neon.key
+	printf "ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key" | sha256sum -c &&
+	apt-key add neon.key
 
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1B69B2DA > /dev/null
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1118213C > /dev/null
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AF1CDFA9 > /dev/null
 
 
-# -- Use optimized sources.list.
+# -- Use sources.list.build to build ISO.
 
 cp /configs/sources.list.build /etc/apt/sources.list
 
@@ -50,6 +50,54 @@ apt -qq update > /dev/null
 apt -yy -qq upgrade > /dev/null
 apt -yy install ${PACKAGES//\\n/ } --no-install-recommends > /dev/null
 apt -yy -qq purge --remove vlc > /dev/null
+
+
+# -- Use sources.list.upgrade to update system.
+
+cp /configs/sources.list.upgrade /etc/apt/sources.list
+
+apt -qq update > /dev/null
+apt -yy -qq dist-upgrade > /dev/null
+
+
+# -- Install AppImage daemon. AppImages that are downloaded to the dirs monitored by the daemon should be integrated automatically.
+# -- firejail should be automatically used by the daemon to sandbox AppImages.
+
+appimgd='
+https://github.com/AppImage/appimaged/releases/download/continuous/appimaged_1-alpha-git2e34378.travis108_amd64.deb
+'
+
+mkdir appimaged_deb
+
+for x in $appimgd; do
+	wget -q -P appimaged_deb $x
+done
+
+dpkg -iR appimaged_deb > /dev/null
+rm -r appimaged_deb
+
+
+# -- Install the latest LTS kernel.
+
+printf "INSTALLING NEW KERNEL."
+
+
+kfiles='
+https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-headers-4.19.35-041935_4.19.35-041935.201904170334_all.deb
+https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-headers-4.19.35-041935-generic_4.19.35-041935.201904170334_amd64.deb
+https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-image-unsigned-4.19.35-041935-generic_4.19.35-041935.201904170334_amd64.deb
+https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-modules-4.19.35-041935-generic_4.19.35-041935.201904170334_amd64.deb
+'
+
+mkdir latest_kernel
+
+for x in $kfiles; do
+	printf "$x"
+	wget -q -P latest_kernel $x
+done
+
+dpkg -iR latest_kernel > /dev/null
+rm -r latest_kernel
 
 
 # -- Add /Applications to $PATH.
@@ -98,23 +146,6 @@ mv /Applications/znx_stable /Applications/znx
 cp /configs/znx-gui.desktop /usr/share/applications
 wget -q -O /bin/znx-gui https://raw.githubusercontent.com/Nitrux/nitrux-iso-tool/development/configs/znx-gui
 chmod +x /bin/znx-gui
-
-
-# -- Install AppImage daemon. AppImages that are downloaded to the dirs monitored by the daemon should be integrated automatically.
-# -- firejail should be automatically used by the daemon to sandbox AppImages.
-
-appimgd='
-https://github.com/AppImage/appimaged/releases/download/continuous/appimaged_1-alpha-git2e34378.travis108_amd64.deb
-'
-
-mkdir appimaged_deb
-
-for x in $appimgd; do
-	wget -q -P appimaged_deb $x
-done
-
-dpkg -iR appimaged_deb > /dev/null
-rm -r appimaged_deb
 
 
 # -- Add config for SDDM.
@@ -167,29 +198,6 @@ cp /configs/vfio-pci.conf /etc/modprobe.d/
 cp /configs/vfio-pci-override-vga.sh /bin/
 
 
-# -- Install the latest LTS kernel.
-
-printf "INSTALLING NEW KERNEL."
-
-
-kfiles='
-https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-headers-4.19.35-041935_4.19.35-041935.201904170334_all.deb
-https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-headers-4.19.35-041935-generic_4.19.35-041935.201904170334_amd64.deb
-https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-image-unsigned-4.19.35-041935-generic_4.19.35-041935.201904170334_amd64.deb
-https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.35/linux-modules-4.19.35-041935-generic_4.19.35-041935.201904170334_amd64.deb
-'
-
-mkdir latest_kernel
-
-for x in $kfiles; do
-	printf "$x"
-	wget -q -P latest_kernel $x
-done
-
-dpkg -iR latest_kernel > /dev/null
-rm -r latest_kernel
-
-
 # -- Add itch.io store launcher.
 
 mkdir -p /etc/skel/.local/share/applications
@@ -208,6 +216,6 @@ apt -yy -qq purge --remove casper lupin-casper > /dev/null
 apt -yy -qq autoremove > /dev/null
 apt -yy -qq clean > /dev/null
 
-# -- Copy end-user sources.list.
+# -- Use sources.list.nitrux for release.
 
 /bin/cp /configs/sources.list.nitrux /etc/apt/sources.list
