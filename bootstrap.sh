d915640c353bff04f5ac84cbe25b4c54c8ebc193
Uri Herrera <hello@uriherrera.com>
2019-05-30 18:08:16 -0500

    Add window title plasmoid

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 
-export LANG=C
-export LC_ALL=C
+printf "\n"
+printf "STARTING BOOTSTRAP."
+printf "\n"
 
 
 # -- Packages to install.
@@ -27,12 +28,13 @@ apt -yy install apt-transport-https wget ca-certificates gnupg2 apt-utils xz-uti
 
 # -- Add key for Neon repository.
 # -- Add key for our repository.
-# -- Add key for the Graphics Driver PPA.
+# -- Add key for the Proprietary Graphics Drivers PPA.
 # -- Add key for the Ubuntu-X PPA.
 
 	wget -q https://archive.neon.kde.org/public.key -O neon.key
 	printf "ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key" | sha256sum -c &&
 	apt-key add neon.key > /dev/null
+	rm neon.key
 
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1B69B2DA > /dev/null
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1118213C > /dev/null
@@ -46,6 +48,10 @@ cp /configs/sources.list.build /etc/apt/sources.list
 
 # -- Update packages list and install packages. Install Nomad Desktop meta package and base-files package avoiding recommended packages.
 
+printf "\n"
+printf "INSTALLING DESKTOP."
+printf "\n"
+
 apt -qq update > /dev/null
 apt -yy -qq upgrade > /dev/null
 apt -yy -qq install ${PACKAGES//\\n/ } --no-install-recommends
@@ -56,8 +62,12 @@ apt -yy -qq dist-upgrade > /dev/null
 # -- Install AppImage daemon. AppImages that are downloaded to the dirs monitored by the daemon should be integrated automatically.
 # -- firejail should be automatically used by the daemon to sandbox AppImages.
 
+printf "\n"
+printf "INSTALLING APPIMAGE DAEMON."
+printf "\n"
+
 appimgd='
-https://github.com/AppImage/appimaged/releases/download/continuous/appimaged_1-alpha-gitff7f673.travis117_amd64.deb
+https://github.com/AppImage/appimaged/releases/download/continuous/appimaged_1-alpha-git23a3b00.travis197_amd64.deb
 '
 
 mkdir appimaged_deb
@@ -66,13 +76,18 @@ for x in $appimgd; do
 	wget -q -P appimaged_deb $x
 done
 
-dpkg -iR appimaged_deb > /dev/null
+dpkg -iR appimaged_deb &> /dev/null
+apt -yy --fix-broken install &> /dev/null
 rm -r appimaged_deb
 
 
-# -- Install the latest LTS kernel.
 
-printf "INSTALLING NEW KERNEL."
+# -- Install the kernel.
+
+printf "\n"
+printf "INSTALLING KERNEL."
+printf "\n"
+
 
 kfiles='
 https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19.39/linux-headers-4.19.39-041939_4.19.39-041939.201905040435_all.deb
@@ -88,10 +103,16 @@ for x in $kfiles; do
 	wget -q -P latest_kernel $x
 done
 
-dpkg -iR latest_kernel > /dev/null
+dpkg -iR latest_kernel &> /dev/null
+dpkg --configure -a &> /dev/null
 rm -r latest_kernel
 
+
 # -- Install util-linux 2.33.1.
+
+printf "\n"
+printf "INSTALLING UTIL-LINUX."
+printf "\n"
 
 util_linux='
 http://mirrors.kernel.org/ubuntu/pool/main/libc/libcap-ng/libcap-ng0_0.7.9-2_amd64.deb
@@ -108,10 +129,16 @@ for x in $util_linux; do
 	wget -q -P util_linux_233 $x
 done
 
-dpkg --force-all -iR util_linux_233 > /dev/null
+dpkg --force-all -iR util_linux_233 &> /dev/null
+dpkg --configure -a &> /dev/null
 rm -r util_linux_233
 
+
 # -- Install libc6 2.29.
+
+printf "\n"
+printf "INSTALLING LIBC6."
+printf "\n"
 
 libc6='
 http://mirrors.kernel.org/ubuntu/pool/main/g/glibc/libc6_2.29-0ubuntu2_amd64.deb
@@ -126,11 +153,16 @@ for x in $libc6; do
 	wget -q -P libc6_229 $x
 done
 
-dpkg --force-all -iR libc6_229 > /dev/null
+dpkg --force-all -iR libc6_229 &> /dev/null
+dpkg --configure -a &> /dev/null
 rm -r libc6_229
 
 
 # -- Add /Applications to $PATH.
+
+printf "\n"
+printf "ADD APPIMAGES."
+printf "\n"
 
 printf "PATH=$PATH:/Applications\n" > /etc/environment
 sed -i "s|secure_path\=.*$|secure_path=\"$PATH:/Applications\"|g" /etc/sudoers
@@ -142,7 +174,7 @@ sed -i "/env_reset/d" /etc/sudoers
 # -- Rename AppImageUpdate and znx.
 
 APPS_SYS='
-https://github.com/Nitrux/znx/releases/download/continuous-stable/znx_stable
+https://github.com/Nitrux/znx/releases/download/continuous-development/znx_development
 https://github.com/AppImage/AppImageUpdate/releases/download/continuous/AppImageUpdate-x86_64.AppImage
 '
 
@@ -156,9 +188,9 @@ chmod +x /Applications/*
 mkdir -p /etc/skel/Applications
 
 APPS_USR='
-http://libreoffice.soluzioniopen.com/stable/basic/LibreOffice-6.2.3-x86_64.AppImage
+http://libreoffice.soluzioniopen.com/stable/basic/LibreOffice-6.2.4-x86_64.AppImage
 http://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/Waterfox-latest-x86_64.AppImage
-https://raw.githubusercontent.com/UriHerrera/storage/master/AppImages/VLC-3.0.0.gitfeb851a.glibc2.17-x86-64.AppImage
+https://github.com/UriHerrera/storage/raw/master/AppImages/VLC-3.0.0.gitfeb851a.glibc2.17-x86-64.AppImage
 '
 
 for x in $APPS_USR; do
@@ -168,9 +200,14 @@ done
 chmod +x /etc/skel/Applications/*
 
 mv /Applications/AppImageUpdate-x86_64.AppImage /Applications/AppImageUpdate
-mv /Applications/znx_stable /Applications/znx
+mv /Applications/znx_development /Applications/znx
+
 
 # -- Add znx-gui.
+
+printf "\n"
+printf "ADD ZNX_GUI."
+printf "\n"
 
 cp /configs/znx-gui.desktop /usr/share/applications
 wget -q -O /bin/znx-gui https://raw.githubusercontent.com/Nitrux/nitrux-iso-tool/development/configs/znx-gui
@@ -182,6 +219,11 @@ chmod +x /bin/znx-gui
 # -- Add kservice menu item for Dolphin for AppImageUpdate.
 # -- Add custom launchers for Maui apps.
 # -- Add policykit file for KDialog.
+# -- Stop kernel printk from flooding the console and other settings.
+
+printf "\n"
+printf "ADD MISC. FIXES."
+printf "\n"
 
 cp /configs/sddm.conf /etc
 cp /configs/10-globally-managed-devices.conf /etc/NetworkManager/conf.d/
@@ -213,8 +255,6 @@ echo "vfio_pci ids=" >> /etc/modules
 cp /configs/asound.conf /etc/
 cp /configs/asound.conf /etc/skel/.asoundrc
 
-cp /configs/initramfs.conf /etc/initramfs-tools/
-
 cp /configs/iommu_unsafe_interrupts.conf /etc/modprobe.d/
 
 cp /configs/amdgpu.conf /etc/modprobe.d/
@@ -233,7 +273,23 @@ mkdir -p /etc/skel/.local/share/applications
 cp /configs/install.itch.io.desktop /etc/skel/.local/share/applications
 cp /configs/install-itch-io.sh /etc/skel/.config
 
+
+# -- Add Window title plasmoid.
+
+printf "\n"
+printf "ADD WINDOW TITLE PLASMOID."
+printf "\n"
+
+cp -a /configs/org.kde.windowtitle /usr/share/plasma/plasmoids
+
+
 # -- Update the initramfs.
+
+printf "\n"
+printf "UPDATE INITRAMFS."
+printf "\n"
+
+cp /configs/initramfs.conf /etc/initramfs-tools/
 
 cat /configs/persistence >> /usr/share/initramfs-tools/scripts/casper-bottom/05mountpoints_lupin
 update-initramfs -u
@@ -241,10 +297,20 @@ update-initramfs -u
 
 # -- Clean the filesystem.
 
+printf "\n"
+printf "REMOVE CASPER."
+printf "\n"
+
 apt -yy -qq purge --remove casper lupin-casper > /dev/null
 apt -yy -qq autoremove
 apt -yy -qq clean > /dev/null
 
+
 # -- Use sources.list.nitrux for release.
 
 /bin/cp /configs/sources.list.nitrux /etc/apt/sources.list
+
+
+printf "\n"
+printf "EXITING BOOTSTRAP."
+printf "\n"
