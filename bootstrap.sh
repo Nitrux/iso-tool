73f6177be18fb71788dc5853c8fb0f47b34a18c6
Uri <hello@uriherrera.com>
2019-06-28 20:00:00 -0500

    order deps

--- bootstrap.sh
+++ bootstrap.sh
@@ -151,7 +151,7 @@ done
 chmod +x /etc/skel/Applications/*
 
 mv /Applications/AppImageUpdate-x86_64.AppImage /Applications/AppImageUpdate
-mv /Applications/znx_master/Applications/znx
+mv /Applications/znx_master /Applications/znx
 
 
 # -- Add znx-gui.
