0c62eb2b47c4aeb91d101c827598e415740e9f34
UriHerrera <hello@uriherrera.com>
2019-08-14 02:46:45 -0500

    missing &

--- bootstrap.sh
+++ bootstrap.sh
@@ -77,7 +77,7 @@ apt -yy -qq upgrade &> /dev/null
 apt -yy -qq install ${DESKTOP_PACKAGES//\\n/ } --no-install-recommends
 apt -yy --fix-broken install
 apt -yy -qq purge --remove vlc &> /dev/null
-apt -yy -qq dist-upgrade > /dev/null
+apt -yy -qq dist-upgrade &> /dev/null
 
 
 # -- Install AppImage daemon. AppImages that are downloaded to the dirs monitored by the daemon should be integrated automatically.
