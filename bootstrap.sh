fae6b1a403228de9ceeb67060c6a96bacd386126
UriHerrera <hello@uriherrera.com>
2019-08-31 00:06:57 -0500

    update grub

--- bootstrap.sh
+++ bootstrap.sh
@@ -201,6 +201,11 @@ apt -qq update
 
 UPGRADE_OS_PACKAGES='
 amd64-microcode
+grub-common
+grub-efi-amd64
+grub-efi-amd64-bin
+grub-efi-amd64-signed
+grub2-common
 i965-va-driver
 initramfs-tools
 initramfs-tools-bin
