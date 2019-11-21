639c97a7f7375084a0490fe39e4c94e13dedf97a
UriHerrera <hello@uriherrera.com>
2019-11-20 21:46:55 -0600

    use 16384 bs

--- build.sh
+++ build.sh
@@ -77,7 +77,7 @@ cp $(echo $BUILD_DIR/initrd* | tr ' ' '\n' | sort | tail -n 1) $ISO_DIR/boot/ini
 (while :; do sleep 300; printf "."; done) &
 
 mkdir -p $ISO_DIR/casper
-mksquashfs $BUILD_DIR $ISO_DIR/casper/filesystem.squashfs -comp gzip -no-progress -b 65536
+mksquashfs $BUILD_DIR $ISO_DIR/casper/filesystem.squashfs -comp gzip -no-progress -b 16384
 
 
 # -- Write the commit hash that generated the image.
