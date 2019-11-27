7e1f9d84b3293c0df136ebb926c028eb64ee4df1
UriHerrera <hello@uriherrera.com>
2019-11-27 05:01:50 -0600

    updt icn path, update npm stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -359,7 +359,7 @@ cp /configs/other/appimagekit-appimaged.desktop /etc/skel/.config/autostart/
 rm -R /usr/share/icons/breeze_cursors /usr/share/icons/Breeze_Snow
 cp -a /configs/other/org.kde.windowtitle /usr/share/plasma/plasmoids
 cp /configs/other/nx-welcome-wizard.desktop /usr/share/applications
-mkdir -p /etc/skel/.local/share/{icons,hicolor,128x128,apps}
+mkdir -p /etc/skel/.local/share/icons/hicolor/128x128/apps
 cp /configs/other/appimagekit_9bc78f4f736b1666c4f9b30bf7c69cd2_waterfox.png /etc/skel/.local/share/icons/hicolor/128x128/apps/
 
 
@@ -525,7 +525,9 @@ printf "\n"
 
 mkdir /etc/skel/.npm-packages
 cp /configs/files/npmrc /etc/skel/.npmrc
-npm install -g nativefier
+npm install npm@latest -g
+exec $SHELL
+npm install nativefier -g
 
 
 # -- Clean the filesystem.
