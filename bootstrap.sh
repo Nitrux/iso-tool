608cf9edffa437a5b32a3d024b8f89202310c0b6
Uri Herrera <hello@uriherrera.com>
2019-12-15 01:28:38 -0600

    rm script
    Doesn't do anything to help

--- bootstrap.sh
+++ bootstrap.sh
@@ -374,7 +374,6 @@ cp /configs/other/znx-gui.desktop /usr/share/applications
 # -- Add welcome wizard to app menu.
 # -- Waterfox-current AppImage is missing an icon the menu, add it for the default user.
 # -- Delete KDE Connect unnecessary menu entries.
-# -- Use script to restart latte-dock on login to load icon cache properly.
 #FIXME These fixes should be included in a package.
 #FIXME This should be included as a deb package downloaded to our repository.
 
@@ -396,7 +395,6 @@ cp /configs/other/nx-welcome-wizard.desktop /usr/share/applications
 mkdir -p /etc/skel/.local/share/icons/hicolor/128x128/apps
 cp /configs/other/appimagekit_9bc78f4f736b1666c4f9b30bf7c69cd2_waterfox.png /etc/skel/.local/share/icons/hicolor/128x128/apps/
 rm /usr/share/applications/org.kde.kdeconnect.sms.desktop /usr/share/applications/org.kde.kdeconnect_open.desktop /usr/share/applications/org.kde.kdeconnect.app.desktop
-cp /configs/script/startnx.sh /bin/startnx
 
 
 # -- Add vfio modules and files.
