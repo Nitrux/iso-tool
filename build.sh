b1ed0cd70d0d9c6e91384bffadbfcc292d0a5540
luis-lavaire <llavaire01@gmail.com>
2020-01-06 21:18:12 -0600

    Updated to make use of new mkiso flags.

--- build.sh
+++ build.sh
@@ -90,13 +90,6 @@ mkdir -p $ISO_DIR/casper
 mksquashfs $BUILD_DIR $ISO_DIR/casper/filesystem.squashfs -comp gzip -no-progress -b 16384
 
 
-# -- Write relevant data to the image.
-
-echo "UPDATE_URL $UPDATE_URL" >> $ISO_DIR/.INFO
-echo "HASH_URL $HASH_URL" >> $ISO_DIR/.INFO
-echo "VERSION ${TRAVIS_COMMIT:0:7}" >> $ISO_DIR/.INFO
-
-
 # -- Generate the ISO image.
 
 wget -qO /bin/mkiso https://raw.githubusercontent.com/Nitrux/tools/master/mkiso
@@ -106,6 +99,11 @@ git clone https://github.com/Nitrux/nitrux-grub-theme grub-theme
 
 mkiso \
 	-V "NITRUX" \
+	-b \
+	-e \
+	-u "$UPDATE_URL" \
+	-s "$HASH_URL" \
+	-r "${TRAVIS_COMMIT:0:7}" \
 	-g $CONFIG_DIR/files/grub.cfg \
 	-g $CONFIG_DIR/files/loopback.cfg \
 	-t grub-theme/nitrux \
