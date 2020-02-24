dd2d74ca53353b1a2f2ef1feee6e32ad6c5b3837
Uri Herrera <hello@uriherrera.com>
2020-02-23 18:31:04 -0600

    fix apt

--- bootstrap.sh
+++ bootstrap.sh
@@ -106,6 +106,7 @@ nx-desktop
 
 apt update &> /dev/null
 apt -yy upgrade
+apt -yy --fix-broken install
 apt -yy install ${DESKTOP_PACKAGES//\\n/ } --no-install-recommends
 apt -yy --fix-broken install &> /dev/null
 apt -yy purge --remove vlc &> /dev/null
