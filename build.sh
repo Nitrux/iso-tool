b0e312a7806cd8694a84690c58cda20aed66208c
Uri Herrera <hello@uriherrera.com>
2020-03-30 21:42:35 -0600

    [ci skip]

--- build.sh
+++ build.sh
@@ -80,7 +80,7 @@ cp $(echo $BUILD_DIR/boot/vmlinuz* | tr ' ' '\n' | sort | tail -n 1) $ISO_DIR/bo
 cp $(echo $BUILD_DIR/boot/initrd* | tr ' ' '\n' | sort | tail -n 1) $ISO_DIR/boot/initramfs
 
 
-# -- Put this file here?
+# -- Put this file here?.
 
 mkdir -p $ISO_DIR/boot/grub/x86_64-efi
 cp /usr/lib/grub/x86_64-efi/linuxefi.mod $ISO_DIR/boot/grub/x86_64-efi
