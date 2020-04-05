2ee9fc68979f04dbe1e63600bd133a4952e3c492
Uri Herrera <hello@uriherrera.com>
2020-04-04 20:06:23 -0600

    build complete image no.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -217,6 +217,202 @@ apt -yy install ${MISC_DEVUAN_PKGS//\\n/ } ${MISC_KDE_PKGS//\\n/ } ${NX_DESKTOP_
 apt -yy --fix-broken install
 
 
+# -- Use sources.list.eaon to update packages and install brew.
+#FIXME We need to provide these packages from a repository of ours.
+
+echo -e "\n"
+echo -e "ADD BREW PACKAGE."
+echo -e "\n"
+
+apt -yy install ${ADD_BREW_PACKAGES//\\n/ } --no-install-recommends
+apt clean &> /dev/null
+apt autoclean &> /dev/null
+
+
+# -- Upgrade KF5 libs for Latte Dock.
+
+echo -e "\n"
+echo -e "UPGRADING KDE PACKAGES."
+echo -e "\n"
+
+cp /configs/files/sources.list.neon.unstable /etc/apt/sources.list.d/neon-unstable-repo.list
+
+HOLD_KDE_PKGS='
+kwin-addons
+kwin-common
+kwin-data
+kwin-x11
+libkwin4-effect-builtins1
+libkwineffects12
+libkwinglutils12
+libkwinxrenderutils12
+libphonon4qt5-4
+qml-module-org-kde-kwindowsystem
+'
+
+UPDT_KDE_PKGS='
+ark
+kcalc
+kde-spectacle
+kdeconnect
+kmenuedit
+kscreen
+latte-dock=0.9.9+p18.04+git20200401.0016-0
+libarchive13
+libkf5activities5
+libkf5activitiesstats1
+libkf5archive5
+libkf5attica5
+libkf5auth-data
+libkf5auth5
+libkf5authcore5
+libkf5baloo5
+libkf5balooengine5
+libkf5bluezqt-data
+libkf5bluezqt6
+libkf5bookmarks-data
+libkf5bookmarks5
+libkf5calendarevents5
+libkf5codecs-data
+libkf5codecs5
+libkf5completion-data
+libkf5completion5
+libkf5config-data
+libkf5configcore5
+libkf5configgui5
+libkf5configwidgets-data
+libkf5configwidgets5
+libkf5contacts-data
+libkf5contacts5
+libkf5coreaddons-data
+libkf5coreaddons5
+libkf5crash5
+libkf5dbusaddons-data
+libkf5dbusaddons5
+libkf5declarative-data
+libkf5declarative5
+libkf5dnssd-data
+libkf5dnssd5
+libkf5doctools5
+libkf5emoticons-data
+libkf5emoticons5
+libkf5filemetadata-data
+libkf5filemetadata3
+libkf5globalaccel-bin
+libkf5globalaccel-data
+libkf5globalaccel5
+libkf5globalaccelprivate5
+libkf5guiaddons5
+libkf5holidays-data
+libkf5holidays5
+libkf5i18n-data
+libkf5i18n5
+libkf5iconthemes-data
+libkf5iconthemes5
+libkf5idletime5
+libkf5itemmodels5
+libkf5itemviews-data
+libkf5itemviews5
+libkf5jobwidgets-data
+libkf5jobwidgets5
+libkf5kcmutils-data
+libkf5kcmutils5
+libkf5kdelibs4support-data
+libkf5kdelibs4support5
+libkf5kiocore5
+libkf5kiofilewidgets5
+libkf5kiogui5
+libkf5kiontlm5
+libkf5kiowidgets5
+libkf5kipi-data
+libkf5kipi32.0.0
+libkf5kirigami2-5
+libkf5modemmanagerqt6
+libkf5networkmanagerqt6
+libkf5newstuff-data
+libkf5newstuff5
+libkf5newstuffcore5
+libkf5notifications-data
+libkf5notifications5
+libkf5notifyconfig-data
+libkf5notifyconfig5
+libkf5package-data
+libkf5package5
+libkf5parts-data
+libkf5parts5
+libkf5people-data
+libkf5people5
+libkf5peoplebackend5
+libkf5peoplewidgets5
+libkf5plasma5
+libkf5plasmaquick5
+libkf5prison5
+libkf5pty-data
+libkf5pty5
+libkf5pulseaudioqt2
+libkf5purpose-bin
+libkf5purpose5
+libkf5quickaddons5
+libkf5runner5
+libkf5service-bin
+libkf5service-data
+libkf5service5
+libkf5solid5
+libkf5solid5-data
+libkf5sonnet5-data
+libkf5sonnetcore5
+libkf5sonnetui5
+libkf5style5
+libkf5su-bin
+libkf5su-data
+libkf5su5
+libkf5syntaxhighlighting-data
+libkf5syntaxhighlighting5
+libkf5texteditor-bin
+libkf5texteditor5
+libkf5textwidgets-data
+libkf5textwidgets5
+libkf5threadweaver5
+libkf5wallet-data
+libkf5wallet5
+libkf5waylandclient5
+libkf5waylandserver5
+libkf5widgetsaddons-data
+libkf5widgetsaddons5
+libkf5windowsystem-data
+libkf5windowsystem5
+libkf5xmlgui-bin
+libkf5xmlgui-data
+libkf5xmlgui5
+polkit-kde-agent-1
+powerdevil
+powerdevil-data
+qml-module-org-kde-draganddrop
+qml-module-org-kde-kcm
+qml-module-org-kde-kconfig
+qml-module-org-kde-kcoreaddons
+qml-module-org-kde-kholidays
+qml-module-org-kde-kio
+qml-module-org-kde-kirigami2
+qml-module-org-kde-kquickcontrols
+qml-module-org-kde-kquickcontrolsaddons
+qml-module-org-kde-newstuff
+qml-module-org-kde-people
+qml-module-org-kde-qqc2desktopstyle
+qml-module-org-kde-quickcharts
+qml-module-org-kde-solid
+qml-module-org-kde-userfeedback
+'
+
+apt update
+apt-mark hold ${HOLD_KDE_PKGS//\\n/ }
+apt -yy install ${UPDT_KDE_PKGS//\\n/ } --only-upgrade --no-install-recommends
+apt -yy --fix-broken install
+apt -yy autoremove
+apt clean &> /dev/null
+apt autoclean &> /dev/null
+
+
 # -- No apt usage past this point. -- #
 #WARNING
 
@@ -247,6 +443,88 @@ dpkg --configure -a &> /dev/null
 rm -r /latest_kernel
 
 
+# -- Install Maui apps Debs.
+# -- Add custom launchers for Maui apps.
+#FIXME This should be synced to our repository.
+
+echo -e "\n"
+echo -e "INSTALLING MAUI APPS."
+echo -e "\n"
+
+mauipkgs='
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/libs/mauikit-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/apps/buho-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/apps/contacts-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/apps/index-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/apps/nota-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/apps/pix-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/apps/station-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/apps/vvave-1.0.0-Linux.deb
+https://raw.githubusercontent.com/UriHerrera/storage/master/Debs/libs/qml-module-qmltermwidget_0.1+git20180903-1_amd64.deb
+'
+
+mkdir /maui_debs
+
+for x in $mauipkgs; do
+	wget -q -P /maui_debs $x
+done
+
+dpkg -iR /maui_debs &> /dev/null
+dpkg --configure -a &> /dev/null
+rm -r /maui_debs
+
+/bin/cp /configs/other/{org.kde.buho.desktop,org.kde.index.desktop,org.kde.nota.desktop,org.kde.pix.desktop,org.kde.station.desktop,org.kde.vvave.desktop,org.kde.contacts.desktop} /usr/share/applications
+whereis index buho nota vvave station pix contacts
+
+
+# -- Add missing firmware modules.
+#FIXME These files should be included in a package.
+
+echo -e "\n"
+echo -e "ADDING MISSING FIRMWARE."
+echo -e "\n"
+
+fw='
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/vega20_ta.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/bxt_huc_ver01_8_2893.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/raven_kicker_rlc.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_asd.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_ce.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_gpu_info.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_me.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_mec.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_mec2.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_pfp.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_rlc.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_sdma.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_sdma1.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_smc.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_sos.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/navi10_vcn.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_asd.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_ce.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_gpu_info.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_me.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_mec.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_mec2.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_pfp.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_rlc.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_sdma.bin
+https://raw.githubusercontent.com/UriHerrera/storage/master/Files/renoir_vcn.bin
+'
+
+mkdir /fw_files
+
+for x in $fw; do
+    wget -q -P /fw_files $x
+done
+
+cp /fw_files/{vega20_ta.bin,raven_kicker_rlc.bin,navi10_*.bin,renoir_*.bin} /lib/firmware/amdgpu/
+cp /fw_files/bxt_huc_ver01_8_2893.bin /lib/firmware/i915/
+
+rm -r /fw_files
+
+
 # -- Add /Applications to $PATH.
 
 echo -e "\n"
@@ -256,6 +534,166 @@ echo -e "\n"
 echo -e "PATH=$PATH:/Applications\n" > /etc/environment
 sed -i "s|secure_path\=.*$|secure_path=\"$PATH:/Applications\"|g" /etc/sudoers
 sed -i "/env_reset/d" /etc/sudoers
+
+
+# -- Add system AppImages.
+# -- Create /Applications directory for users.
+# -- Rename AppImages for easy access from the terminal.
+
+echo -e "\n"
+echo -e "ADD APPIMAGES."
+echo -e "\n"
+
+APPS_SYS='
+https://raw.githubusercontent.com/UriHerrera/storage/master/AppImages/znx-master-x86_64.AppImage
+https://github.com/Nitrux/znx-gui/releases/download/continuous-stable/znx-gui_master-x86_64.AppImage
+https://github.com/AppImage/AppImageUpdate/releases/download/continuous/AppImageUpdate-x86_64.AppImage
+https://raw.githubusercontent.com/UriHerrera/storage/master/AppImages/appimage-cli-tool-x86_64.AppImage
+https://raw.githubusercontent.com/UriHerrera/storage/master/AppImages/pnx-1.0.0-x86_64.AppImage
+https://raw.githubusercontent.com/UriHerrera/storage/master/Binaries/vmetal-free-amd64
+https://github.com/ferion11/Proton_Appimage/releases/download/continuous/proton-linux-x86-v4.2-PlayOnLinux-x86_64.AppImage
+https://github.com/AppImage/appimaged/releases/download/continuous/appimaged-x86_64.AppImage
+'
+
+APPS_USR='
+https://files.kde.org/kdenlive/release/kdenlive-19.12.3-x86_64.appimage
+https://libreoffice.soluzioniopen.com/stable/fresh/LibreOffice-fresh.basic-x86_64.AppImage
+https://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/AppImage/waterfox-classic-latest-x86_64.AppImage
+https://github.com/aferrero2707/gimp-appimage/releases/download/continuous/GIMP_AppImage-git-2.10.19-20200323-x86_64.AppImage
+https://raw.githubusercontent.com/UriHerrera/storage/master/AppImages/mpv-0.30.0-x86_64.AppImage
+https://raw.githubusercontent.com/UriHerrera/storage/master/AppImages/Inkscape-0.92.3+68.glibc2.15-x86_64.AppImage
+https://github.com/LMMS/lmms/releases/download/v1.2.1/lmms-1.2.1-linux-x86_64.AppImage
+'
+
+mkdir /Applications
+mkdir -p /etc/skel/Applications
+mkdir -p /etc/skel/.local/bin
+
+for x in $APPS_SYS; do
+    wget -q -P /Applications $x
+done
+
+for x in $APPS_USR; do
+    wget -q -P /Applications $x
+done
+
+chmod +x /Applications/*
+
+mv /Applications/znx-master-x86_64.AppImage /Applications/znx
+mv /Applications/znx-gui_master-x86_64.AppImage /Applications/znx-gui
+mv /Applications/AppImageUpdate-x86_64.AppImage /Applications/appimageupdate
+mv /Applications/appimage-cli-tool-x86_64.AppImage /Applications/app
+mv /Applications/pnx-1.0.0-x86_64.AppImage /Applications/pnx
+mv /Applications/vmetal-free-amd64 /Applications/vmetal
+mv /Applications/proton-linux-x86-v4.2-PlayOnLinux-x86_64.AppImage /Applications/wine
+
+ln -sv /Applications/wine /Applications/wineserver
+
+mv /Applications/appimaged-x86_64.AppImage /etc/skel/.local/bin/appimaged
+
+mv /Applications/LibreOffice-fresh.basic-x86_64.AppImage /Applications/libreoffice
+mv /Applications/waterfox-classic-latest-x86_64.AppImage /Applications/waterfox
+mv /Applications/kdenlive-*-x86_64.appimage /Applications/kdenlive
+mv /Applications/GIMP_AppImage-*-x86_64.AppImage /Applications/gimp
+mv /Applications/mpv-*-x86_64.AppImage /Applications/mpv
+mv /Applications/Inkscape-0.92.3+68.glibc2.15-x86_64.AppImage /Applications/inkscape
+mv /Applications/lmms-1.2.1-linux-x86_64.AppImage /Applications/lmms
+
+ls -l /Applications
+ls -l /etc/skel/.local/bin/
+
+
+# -- Add AppImage providers for appimage-cli-tool
+
+echo -e "\n"
+echo -e "ADD APPIMAGE PROVIDERS."
+echo -e "\n"
+
+cp /configs/files/appimage-providers.yaml /etc/
+
+
+# -- Add config for SDDM.
+# -- Add fix for https://bugs.launchpad.net/ubuntu/+source/network-manager/+bug/1638842.
+# -- Add kservice menu item for Dolphin for AppImageUpdate.
+# -- Add policykit file for KDialog.
+# -- Add VMetal desktop launcher.
+# -- Add appimaged launcher to autostart.
+# -- Overwrite Qt settings file. This file was IN a package but caused installation conflicts with kio-extras.
+# -- Overwrite Plasma 5 notification positioning. This file was IN a package but caused installation conflicts with plasma-workspace.
+# -- For a strange reason, the Breeze cursors override some of our cursor assets. Delete them from the system to avoid this.
+# -- Add Window title plasmoid.
+# -- Add welcome wizard to app menu.
+# -- Waterfox-current AppImage is missing an icon the menu, add it for the default user.
+# -- Delete KDE Connect unnecessary menu entries.
+# -- Add znx-gui desktop launcher.
+# -- Remove Kinfocenter desktop launcher. The SAME package installs both, the KCM AND the standalone app (why?).
+# -- Remove htop and nsnake desktop launcher.
+# -- Remove ibus-setup desktop launcher and the flipping emojier launcher.
+# -- Copy offline documentation to desktop folder.
+#FIXME These fixes should be in a package.
+
+echo -e "\n"
+echo -e "ADD MISC. FIXES."
+echo -e "\n"
+
+cp /configs/files/sddm.conf /etc
+cp /configs/files/10-globally-managed-devices.conf /etc/NetworkManager/conf.d/
+cp /configs/other/appimageupdate.desktop /usr/share/kservices5/ServiceMenus/
+cp /configs/files/org.freedesktop.policykit.kdialog.policy /usr/share/polkit-1/actions/
+cp /configs/other/vmetal.desktop /usr/share/applications
+cp /configs/other/appimagekit-appimaged.desktop /etc/skel/.config/autostart/
+/bin/cp /configs/files/Trolltech.conf /etc/xdg/Trolltech.conf
+/bin/cp /configs/files/plasmanotifyrc /etc/xdg/plasmanotifyrc
+rm -R /usr/share/icons/breeze_cursors /usr/share/icons/Breeze_Snow
+cp -a /configs/other/org.kde.windowtitle /usr/share/plasma/plasmoids
+cp -a /configs/other/org.kde.video /usr/share/plasma/wallpapers
+cp /configs/other/nx-welcome-wizard.desktop /usr/share/applications
+mkdir -p /etc/skel/.local/share/icons/hicolor/128x128/apps
+rm /usr/share/applications/org.kde.kdeconnect.sms.desktop /usr/share/applications/org.kde.kdeconnect_open.desktop /usr/share/applications/org.kde.kdeconnect.app.desktop
+cp /configs/other/znx-gui.desktop /usr/share/applications
+/bin/cp /configs/other/org.kde.kinfocenter.desktop /usr/share/applications/org.kde.kinfocenter.desktop
+rm /usr/share/applications/htop.desktop /usr/share/applications/mc.desktop /usr/share/applications/mcedit.desktop /usr/share/applications/nsnake.desktop
+ln -sv /usr/games/nsnake /bin/nsnake
+rm /usr/share/applications/ibus-setup* /usr/share/applications/org.freedesktop.IBus* /usr/share/applications/org.kde.plasma.emojier.desktop /usr/share/applications/info.desktop
+mkdir -p /etc/skel/Desktop
+cp /configs/other/compendium_offline.pdf /etc/skel/Desktop/Nitrux\ —\ Compendium.pdf
+cp /configs/other/faq_offline.pdf /etc/skel/Desktop/Nitrux\ —\ FAQ.pdf
+
+
+# -- Workarounds for PNX.
+#FIXME These need to be fixed in PNX.
+
+echo -e "\n"
+echo -e "ADD WORKAROUNDS FOR PNX."
+echo -e "\n"
+
+mkdir -p /var/lib/pacman/
+mkdir -p /etc/pacman.d/
+mkdir -p /usr/share/pacman/keyrings
+
+cp /configs/files/pacman.conf /etc
+cp /configs/files/mirrorlist /etc/pacman.d
+cp -r /configs/other/pacman/* /usr/share/pacman/keyrings
+
+ln -sv /home/.pnx/usr/lib/dri /usr/lib/dri
+ln -sv /home/.pnx/usr/lib/pulseaudio /usr/lib/pulseaudio
+ln -sv /home/.pnx/usr/lib/gdk-pixbuf-2.0 /usr/lib/gdk-pixbuf-2.0
+ln -sv /home/.pnx/usr/lib/gs-plugins-13 /usr/lib/gs-plugins-13
+ln -sv /home/.pnx/usr/lib/liblmdb.so /usr/lib/liblmdb.so
+ln -sv /home/.pnx/usr/lib/systemd /usr/lib/systemd
+ln -sv /home/.pnx/usr/lib/samba /usr/lib/samba
+ln -sv /home/.pnx/usr/lib/girepository-1.0 /usr/lib/girepository-1.0
+ln -sv /home/.pnx/usr/lib/tracker-2.0 /usr/lib/tracker-2.0
+ln -sv /home/.pnx/usr/lib/WebKitNetworkProcess /usr/lib/WebKitNetworkProcess
+ln -sv /home/.pnx/usr/lib/epiphany /usr/lib/epiphany
+ln -sv /home/.pnx/usr/lib/opera /usr/lib/opera
+ln -sv /home/.pnx/usr/lib/firefox /usr/lib/firefox
+ln -sv /home/.pnx/usr/share/tracker /usr/share/tracker
+ln -sv /home/.pnx/usr/share/xonotic /usr/share/xonotic
+
+mkdir -p /usr/lib/zsh/5.8/zsh/
+
+ln -sv /home/.pnx/usr/lib/zsh/5.8/zsh/datetime.so /usr/lib/zsh/5.8/zsh/datetime.so
 
 
 # -- Add vfio modules and files.
@@ -294,6 +732,45 @@ cp /configs/files/{amdgpu.conf,i915.conf,kvm.conf,nvidia.conf,qemu-system-x86.co
 cp /configs/scripts/{vfio-pci-override-vga.sh,dummy.sh} /bin/
 
 chmod a+x /bin/dummy.sh /bin/vfio-pci-override-vga.sh
+
+
+# -- Add itch.io store launcher.
+#FIXME This should be in a package.
+
+echo -e "\n"
+echo -e "ADD ITCH.IO LAUNCHER."
+echo -e "\n"
+
+
+mkdir -p /etc/skel/.local/share/applications
+cp /configs/other/install.itch.io.desktop /etc/skel/.local/share/applications
+cp /configs/scripts/install-itch-io.sh /etc/skel/.config
+
+
+# -- Add configuration for npm to install packages in home and without sudo and update it.
+# -- Delete 'travis' folder that holds npm cache during build.
+#FIXME This should be in a package.
+
+echo -e "\n"
+echo -e "ADD NPM INSTALL WITHOUT SUDO AND UPDATE IT."
+echo -e "\n"
+
+mkdir /etc/skel/.npm-packages
+cp /configs/files/npmrc /etc/skel/.npmrc
+npm install npm@latest -g
+rm -r /home/travis
+
+
+# -- Add nativefier launcher.
+#FIXME This should be in a package.
+
+echo -e "\n"
+echo -e "ADD NATIVEFIER LAUNCHER."
+echo -e "\n"
+
+
+cp /configs/other/install.nativefier.desktop /etc/skel/.config/autostart/
+cp /configs/scripts/install-nativefier.sh /etc/skel/.config
 
 
 # -- Add oh my zsh.
