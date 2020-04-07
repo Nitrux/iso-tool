66ec76d458b0de5448080d78ba12cd2e3d02687d
Uri Herrera <hello@uriherrera.com>
2020-04-07 01:38:32 -0500

    add xdm file for openrc, add correct script to initramfs

--- configs/scripts/hook-scripts.sh
+++ configs/scripts/hook-scripts.sh
@@ -15,5 +15,5 @@ esac
  . /usr/share/initramfs-tools/hook-functions
 # Begin real processing below this line
 
- copy_exec /bin/dummy.sh /bin/vfio-pci-override-vga.sh
+ copy_exec /bin/vfio-pci-override-vga.sh /bin/vfio-pci-override-vga.sh
  
