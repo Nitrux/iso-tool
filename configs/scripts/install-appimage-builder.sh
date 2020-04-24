26dfca8d13a0b428121adcaf9b77b7ef5fc07ac4
Uri Herrera <hello@uriherrera.com>
2020-04-24 00:06:36 -0500

    use -f instead of -d

--- configs/scripts/install-appimage-builder.sh
+++ configs/scripts/install-appimage-builder.sh
@@ -8,7 +8,7 @@ pip3 install --user appimage-builder
 
 # -- Check if appimage-builder exists then delete script file.
 
-[ -d ~/.local/bin/appimage-builder ] && rm ~/.config/install-appimage-builder.sh ~/.local/share/applications/install.appaimge-builder.desktop
+[ -f ~/.local/bin/appimage-builder ] && rm ~/.config/install-appimage-builder.sh ~/.local/share/applications/install.appaimge-builder.desktop
 
 exit
 
