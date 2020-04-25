8100d20438e06a12afe7bbac33643e28d035b24e
Uri Herrera <hello@uriherrera.com>
2020-04-24 20:41:13 -0500

    don't rm these files

--- configs/scripts/rm-dpkg.sh
+++ configs/scripts/rm-dpkg.sh
@@ -13,13 +13,11 @@ REMOVE_FILES='
 /usr/bin/dpkg-deb
 /usr/bin/dpkg-divert
 /usr/bin/dpkg-maintscript-helper
-/usr/bin/dpkg-query
 /usr/bin/dpkg-split
 /usr/bin/dpkg-statoverride
 /usr/bin/dpkg-trigger
 /usr/bin/update-alternatives
 /usr/share/doc/dpkg/
-/usr/share/dpkg/
 /usr/share/lintian/overrides/dpkg
 /usr/share/polkit-1/actions/org.dpkg.pkexec.update-alternatives.policy
 '
