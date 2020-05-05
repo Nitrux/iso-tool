acb7b0c93748afbe7f104f183aa5f4c3475b8687
Uri Herrera <hello@uriherrera.com>
2020-05-05 04:11:21 -0500

    try with lz4 comp

--- build.sh
+++ build.sh
@@ -95,7 +95,7 @@ cp /usr/lib/grub/x86_64-efi/linuxefi.mod $ISO_DIR/boot/grub/x86_64-efi
 (while :; do sleep 300; printf "."; done) &
 
 mkdir -p $ISO_DIR/casper
-mksquashfs $BUILD_DIR $ISO_DIR/casper/filesystem.squashfs -comp gzip -no-progress -b 1048576
+mksquashfs $BUILD_DIR $ISO_DIR/casper/filesystem.squashfs -comp lz4 -no-progress -b 1048576
 
 
 # -- Generate the ISO image.
