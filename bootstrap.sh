fdd558e8933101379a56ca14726b72a1568c355e
Anupam Basak <anupam.basak27@gmail.com>
2020-05-13 14:50:34 +0530

    Download stable maui AppImages

--- bootstrap.sh
+++ bootstrap.sh
@@ -416,14 +416,18 @@ puts "ADDING MAUI APPS."
 wget -q https://dl.min.io/client/mc/release/linux-amd64/mc -O /tmp/mc
 chmod +x /tmp/mc
 /tmp/mc config host add nx $NITRUX_STORAGE_URL $NITRUX_STORAGE_ACCESS_KEY $NITRUX_STORAGE_SECRET_KEY
-_latest=$(/tmp/mc ls nx/maui/nightly | grep -Po "\d{4}-\d{2}-\d{2}/" | sort -r | head -n 1)
 mkdir maui_pkgs
 
 (
 	cd maui_pkgs
 
-	/tmp/mc cp -r "nx/maui/nightly/$_latest" ./
-
+	_apps=$(mc ls nx/maui/stable/ | grep -Eo "\w*/")
+	
+	for i in $_apps; do
+        _branch=$(/tmp/mc cat nx/maui/stable/${i}LATEST)
+        /tmp/mc cp -r nx/maui/stable/${i}${_branch} ./
+	done
+	
 	mv index-*amd64*.AppImage /Applications/index
 	mv buho-*amd64*.AppImage /Applications/buho
 	mv nota-*amd64*.AppImage /Applications/nota
@@ -435,6 +439,8 @@ mkdir maui_pkgs
 
 	ls -l /Applications
 )
+
+/tmp/mc config host rm nx
 
 rm -r \
 	maui_pkgs \
