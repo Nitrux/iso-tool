326e6b8379f73f504184533af9336fa20853a7bd
Luis Lavaire <llavaire01@gmail.com>
2020-07-08 21:35:46 -0600

    updated vfio-pci script.

--- configs/scripts/vfio-pci-override-vga.sh
+++ configs/scripts/vfio-pci-override-vga.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
+
+grep -q 'iommu=pt' /proc/cmdline ||
+	exit
+
 for i in $(find /sys/devices/pci* -name boot_vga); do
-        GPU=$(dirname $i)
-        
-        if [ $(cat $i) -eq 0 ]; then
-                AUDIO=$(echo $GPU | sed -e "s/0$/1/")
-                echo "vfio-pci" > $GPU/driver_override
-                if [ -d $AUDIO ]; then
-                        echo "vfio-pci" > $AUDIO/driver_override
-                fi
-        else
-                pci_device_id="$(echo "$i"| tr "/" "\n" | tail -n -2 | head -n 1)"
-                pci_device_info="$(lspci -s $pci_device_id)"
-                
-                if echo "$pci_device_info" | grep -qi nvidia; then
-                    echo "nvidia" > $GPU/driver_override
-                elif echo "$pci_device_info" | grep -qi intel; then
-                    echo "i915" > $GPU/driver_override
-                elif echo "$pci_device_info" | grep -qi amd; then
-                    echo "amdgpu" > $GPU/driver_override
-                fi
-        fi
+	GPU="${i%/*}"
+	AUDIO="${GPU%.0}.1"
+
+	grep -q 0 "$i" && {
+		echo "vfio-pci" > "$GPU"/driver_override
+		test -d "$AUDIO" && echo "vfio-pci" > "$AUDIO"/driver_override
+	} || {
+		grep -q vfio-pci "$GPU"/driver ||
+			continue
+
+		dev=$(echo "$i" | grep -Eo '[0-9]{4}:[0-9]{2}:[0-9]{2}\.[0-9]')
+
+		lspci -s "$dev" |
+			grep -Ei 'nvidia|amd' |
+			sed 's/amd/amdgpu/' > "$GPU"/driver_override
+	}
 done
 
 modprobe -i vfio-pci
-
-exit 0
