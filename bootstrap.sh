b35e904243c607f05871ecb08d24d53492b22e1f
Uri Herrera <hello@uriherrera.com>
2020-07-19 02:05:16 -0500

    ffs

--- bootstrap.sh
+++ bootstrap.sh
@@ -183,7 +183,7 @@ puts "INSTALLING BASE SYSTEM."
 GRUB_PACKAGES='
 	grub-efi-amd64-signed=1+2.04+8
 	grub-efi-amd64-bin=2.04-8
-	grub-common=2.04-9
+	grub-common=2.04-8
 	'
 
 NITRUX_BASE_PACKAGES='
