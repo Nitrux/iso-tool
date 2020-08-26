f233006e6ee9eb36077d49595bf018564654e17a
Uri Herrera <hello@uriherrera.com>
2020-08-26 18:45:21 -0500

    change output file name

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ config_dir=$PWD/configs
 
 #	The name of the ISO image.
 
-image=nitrux-$(printf "$TRAVIS_BRANCH\n" | sed "s/master/stable/")-amd64.iso
+image=nitrux-$(printf "$TRAVIS_BRANCH\n" | sed "s/legacy/release/")-amd64.iso
 update_url=http://repo.nxos.org:8000/${image%.iso}.zsync
 hash_url=http://repo.nxos.org:8000/${image%.iso}.md5sum
 
