b851850ddf13af0f7a3b8e0dfa46012480473ae3
Uri Herrera <hello@uriherrera.com>
2021-03-29 17:48:18 -0600

    fix wrong link source

--- bootstrap.sh
+++ bootstrap.sh
@@ -614,8 +614,8 @@ cat /configs/files/casper.conf > /etc/casper.conf
 rm \
 	/boot/{vmlinuz,initrd.img,vmlinuz.old,initrd.img.old} || true
 
-ln -svf /boot/vmlinuz-5.10.19-051019-generic /vmlinuz
-ln -svf /boot/initrd.img-5.10.19-051019-generic /initrd.img
+ln -svf /boot/vmlinuz-5.4.108-0504108-generic /vmlinuz
+ln -svf /boot/initrd.img-5.4.108-0504108-generic /initrd.img
 
 dpkg_force_remove dash || true
 
