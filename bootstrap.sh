574396dd8def7afdd2f8ae4f47b1ebc6e7f0c082
Uri Herrera <hello@uriherrera.com>
2021-06-29 04:31:56 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -573,8 +573,8 @@ cat /configs/files/casper.conf > /etc/casper.conf
 rm \
 	/boot/{vmlinuz,initrd.img,vmlinuz.old,initrd.img.old} || true
 
-ln -svf /boot/vmlinuz-5.12.11-051211-generic /vmlinuz
-ln -svf /boot/initrd.img-5.12.11-051211-generic /initrd.img
+ln -svf /boot/vmlinuz-5.13.0-051300-generic /vmlinuz
+ln -svf /boot/initrd.img-5.13.0-051300-generic /initrd.img
 
 dpkg_force_remove dash || true
 
