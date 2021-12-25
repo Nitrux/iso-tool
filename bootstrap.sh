7f27d1a2c9ff089295e9f4c6526ddec613e22d09
UriHerrera <hello@uriherrera.com>
2021-12-25 16:58:22 -0600

    upgrade pkg

--- bootstrap.sh
+++ bootstrap.sh
@@ -324,7 +324,7 @@ update
 puts "ADDING NITRUX BASE."
 
 NITRUX_BASE_PKGS='
-	base-files=13.1.4+nitrux-legacy
+	base-files=13.1.5+nitrux-legacy
 	nitrux-hardware-drivers-legacy
 	nitrux-minimal-legacy
 	nitrux-standard-legacy
