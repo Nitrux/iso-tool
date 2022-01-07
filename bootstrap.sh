3ddada80271ab5af067315b2ffd8451b1b1fad3c
UriHerrera <hello@uriherrera.com>
2022-01-06 22:18:15 -0600

    blk pkgs

--- bootstrap.sh
+++ bootstrap.sh
@@ -284,27 +284,6 @@ NX_DESKTOP_PKG='
 
 MISC_KDE_PKGS='
 	latte-dock
-	plasma-desktop
-	plasma-widgets-addons
-	kaccounts-integration
-	kaccounts-providers
-	kio-gdrive
-	qml-module-qtwebview
-	fwupd
-	nx-clock-applet
-	nx-desktop-apps
-	nx-firewall
-	nx-notifications-applet
-	nx-simplemenu-applet
-	nx-systemtray-applet
-	signon-ui-x11
-	libqt5webengine5
-	libqt5webenginecore5
-	libkpimgapicore5
-	libkpimgapidrive5
-	plasma-workspace
-	touchegg-kde
-	qml-module-qtwebengine
 '
 
 MISC_DESKTOP_PKGS='
