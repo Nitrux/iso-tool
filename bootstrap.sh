6b909a1f956f4f6e6c581ad52a08aa62fdb8f975
Uri Herrera <hello@uriherrera.com>
2022-01-13 05:04:34 -0600

    chk

--- bootstrap.sh
+++ bootstrap.sh
@@ -301,7 +301,7 @@ PLYMOUTH_DAEDALUS_PKGS='
 	plymouth-x11/daedalus
 '
 
-install plasma-integration plasma-widgets-addons plasma-desktop breeze ibus-table-emoji
+install plasma-integration plasma-widgets-addons plasma-desktop breeze ibus-table-emoji ibus-table
 install $NX_DESKTOP_PKG $MISC_KDE_PKGS $MISC_DESKTOP_PKGS $PLYMOUTH_DAEDALUS_PKGS
 
 
