648eb9d85f573e61f1036441f6cf2b91f840a5c0
Uri Herrera <hello@uriherrera.com>
2022-05-31 14:46:08 -0500

    mv apt funcs to script, add pkg

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,43 +9,7 @@ puts () { printf "\n\n --- %s\n" "$*"; }
 
 #	Wrap APT commands in functions.
 
-add_nitrux_key_repo () { curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -; }
-add_nitrux_key_compat () { curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -; }
-add_nitrux_key_testing () { curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -; }
-add_repo_keys () { apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $@; }
-appstream_refresh_force () { appstreamcli refresh --force; }
-autoremove () { apt -yy autoremove $@; }
-clean_all () { apt clean && apt autoclean; }
-dist_upgrade () { apt -yy dist-upgrade $@; }
-download () { apt download $@; }
-dpkg_force_install () { dpkg --force-all -i $@; }
-dpkg_force_remove () { /usr/bin/dpkg --remove --no-triggers --force-remove-essential --force-bad-path $@; }
-dpkg_install () { dpkg -i $@; }
-fix_install () { apt -yy --fix-broken install $@; }
-fix_install_no_recommends () { apt -yy --fix-broken install --no-install-recommends $@; }
-hold () { apt-mark hold $@; }
-install () { apt -yy install --no-install-recommends $@; }
-install_downgrades () { apt -yy install --no-install-recommends --allow-downgrades $@; }
-install_downgrades_hold () { apt -yy install --no-install-recommends --allow-downgrades --allow-change-held-packages $@; }
-install_force_overwrite () { apt -yy install --no-install-recommends -o Dpkg::Options::="--force-overwrite" $@; }
-install_hold () { apt -yy install --no-install-recommends $@ && apt-mark hold $@; }
-list_installed_apt () { apt list --installed; }
-list_installed_dpkg () { dpkg --list '*'; }
-list_installed_pkgs () { dpkg-query -l | less > installed_pkgs.txt; }
-list_number_pkgs () { dpkg-query -f '${binary:Package}\n' -W | wc -l; }
-list_pkgs_size () { dpkg-query --show --showformat='${Installed-Size}\t${Package}\n' | sort -rh | head -25 | awk '{print $1/1024, $2}'; }
-list_upgrade () { apt list --upgradable; }
-only_upgrade () { apt -yy install --no-install-recommends --only-upgrade $@; }
-only_upgrade_force_overwrite () { apt -yy install --no-install-recommends --only-upgrade -o Dpkg::Options::="--force-overwrite" $@; }
-pkg_policy () { apt-cache policy $@; }
-pkg_search () { apt-cache search $@; }
-purge () { apt -yy purge --remove $@; }
-remove_keys () { apt-key del $@; }
-unhold () { apt-mark unhold $@; }
-update () { apt update; }
-update_quiet () { apt -qq update; }
-upgrade () { apt -yy upgrade $@; }
-upgrade_downgrades () { apt -yy upgrade --allow-downgrades $@; }
+source /configs/scripts/apt_funcs.sh
 
 
 puts "STARTING BOOTSTRAP."
@@ -380,7 +344,7 @@ update
 puts "ADDING NITRUX BASE."
 
 NITRUX_BASE_PKGS='
-	base-files=13.1.13+nitrux-legacy
+	base-files=13.1.14+nitrux-legacy
 	nitrux-minimal-legacy
 	nitrux-standard-legacy
 '
@@ -431,6 +395,7 @@ NX_DESKTOP_PKG='
 
 MISC_DESKTOP_PKGS='
 	dialog
+	dmcrypt-openrc
 '
 
 install_downgrades $NX_DESKTOP_PKG $MISC_DESKTOP_PKGS
