96d194acdf98a611c17734426b46f4312db25c24
Uri Herrera <hello@uriherrera.com>
2023-01-13 09:09:31 -0600

    organize stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,576 +1,197 @@
 #! /bin/bash
 
-set -xe
+set -e
 
 export LANG=C
 export LC_ALL=C
 export DEBIAN_FRONTEND=noninteractive
+export PATH=$PATH:/tmp/scripts
 
 puts () { printf "\n\n --- %s\n" "$*"; }
 
-#	Wrap APT commands in functions.
 
-source /configs/scripts/apt_funcs
+puts "HELLO. STARTING BOOTSTRAP."
 
 
-puts "STARTING BOOTSTRAP."
+#	Source APT commands as functions.
+#	shellcheck source=/dev/null
+
+source /configs/scripts/stages/00-apt-funcs
 
 
 #	Block installation of some packages.
 
-cp /configs/files/preferences /etc/apt/preferences
+cp -r /configs/files/preferences /etc/apt/preferences
+
+
+#	Make directories in /tmp.
+
+mkdir -p /tmp/{kernel,casper,scripts,pkg-origin}
+
+
+#	Copy build scripts.
+
+cp -r /configs/scripts/stages/* /tmp/scripts
+
+
+#	Copy pkg-origin to $PATH
+
+cp -r /configs/files/other/pkg-origin /tmp/pkg-origin
+
+
+#	Check installed packages at start.
+
+list_number_pkgs
+list_installed_text_start
+
+
+puts "---------------------------"
+puts "POPULATING ROOT DIRECTORY."
+
+
+############################
+#	INFORMATION:			#
+#	Root content goes here	#
+############################
 
 
 #	Install basic packages.
 
 puts "ADDING BASIC PACKAGES."
 
-CHROOT_BASIC_PKGS='
-	apt-transport-https
-	apt-utils
-	appstream
-	axel
-	ca-certificates
-	curl
-	dhcpcd5
-	dirmngr
-	gnupg2
-	libzstd-dev
-	lz4
-	zstd
-'
-
-update
-install $CHROOT_BASIC_PKGS
+01-install-basic-pkgs
 
 
-#	Add key for Nitrux repository.
+#	Add repositories to build root.
 
-puts "ADDING REPOSITORY KEYS."
+puts "ADDING BUILD SOURCES."
 
-add_nitrux_key_repo
-add_nitrux_key_compat
-add_nitrux_key_testing
-
-
-#	Copy repository sources.
-
-puts "ADDING SOURCES FILES."
-
-cp /configs/files/sources.list.nitrux /etc/apt/sources.list
-cp /configs/files/sources.list.nitrux.testing /etc/apt/sources.list.d/nitrux-testing-repo.list
-cp /configs/files/sources.list.debian.unstable /etc/apt/sources.list.d/debian-unstable-repo.list
-
-apt-key export 86A634D7 | gpg --dearmour -o /usr/share/keyrings/nitrux-repo.gpg
-apt-key export 712260DE | gpg --dearmour -o /usr/share/keyrings/nitrux-compat.gpg
-apt-key export EB1BEB0D | gpg --dearmour -o /usr/share/keyrings/nitrux-testing.gpg
-
-update
+02-add-build-sources
 
 
 #	Upgrade dpkg for zstd support.
 
-UPGRADE_DPKG='
-	dpkg=1.21.1ubuntu1
-'
+puts "ADDING UPGRADED DPKG."
 
-install_downgrades $UPGRADE_DPKG
-
-
-#	Do dist-upgrade.
-
-dist_upgrade
+03-upgrade-dpkg
 
 
 #	Add bootloader.
-#
-#	The GRUB2 packages from Debian do not work correctly with EFI.
 
-puts "ADDING BOOTLOADER."
+puts "ADDING BOOTLOADER AND SECURE BOOT."
 
-GRUB2_PKGS='
-	grub-common/trixie
-	grub-efi-amd64/trixie
-	grub-efi-amd64-bin/trixie
-	grub-efi-amd64-signed/trixie
-	grub-pc-bin/trixie
-	grub2-common/trixie
-	libfreetype6/unstable
-'
-
-install $GRUB2_PKGS
-
-
-#	Add packages for secure boot compatibility.
-
-puts "ADDING SECURE BOOT COMPAT."
-
-SB_SHIM_PKGS='
-	sbsigntool/trixie
-	shim-signed/trixie
-	mokutil/trixie
-'
-
-install $SB_SHIM_PKGS
+04-install-bootloader
 
 
 #	Add eudev, elogind, and systemctl to replace systemd and utilize other inits.
-#
-#	To remove systemd, we have to replace libsystemd0, udev, elogind and provide systemctl. However, neither of them
-#	are available to install from other sources than Devuan except for systemctl.
 
-add_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
+puts "INSTALLING EUDEV AND ELOGIND."
 
-cp /configs/files/sources.list.devuan.beowulf /etc/apt/sources.list.d/devuan-beowulf-repo.list
-
-update
-
-puts "ADDING EUDEV AND ELOGIND."
-
-DEVUAN_EUDEV_ELOGIND_PKGS='
-	eudev
-	elogind
-'
-
-REMOVE_SYSTEMD_PKGS='
-	systemd
-	systemd-sysv
-	libsystemd0
-'
-
-SYSTEMCTL_STANDALONE_PKG='
-	systemctl
-'
-
-install $DEVUAN_EUDEV_ELOGIND_PKGS
-purge $REMOVE_SYSTEMD_PKGS
-autoremove
-install $SYSTEMCTL_STANDALONE_PKG
-
-rm \
-	/etc/apt/sources.list.d/devuan-beowulf-repo.list
-
-remove_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
-
-update
+05-install-elogind-eudev
 
 
 #	Add OpenRC as init.
 
 puts "ADDING OPENRC AS INIT."
 
-OPENRC_INIT_PKGS='
-	initscripts
-	init-system-helpers
-	openrc
-	policycoreutils
-	startpar
-	sysvinit-utils
-'
-
-install $OPENRC_INIT_PKGS
+06-install-openrc
 
 
 #	Add casper.
-#
-#	It's worth noting that casper isn't available anywhere but Ubuntu.
-#	Debian doesn't use it; it uses live-boot, live-config, et. al.
 
 puts "ADDING CASPER."
 
-CASPER_DEPS_PKGS='
-	casper/trixie
-	initramfs-tools/trixie
-	initramfs-tools-bin/trixie
-	initramfs-tools-core/trixie
-'
-
-install_downgrades $CASPER_DEPS_PKGS
-
-
-#	Hold initramfs and casper packages.
-
-INITRAMFS_CASPER_PKGS='
-	casper
-	initramfs-tools
-	initramfs-tools-core
-	initramfs-tools-bin
-'
-
-hold $INITRAMFS_CASPER_PKGS
+07-install-casper
 
 
 #	Add kernel.
 
-add_repo_keys \
-	9352A0B69B72E6DF > /dev/null
-
-cp /configs/files/sources.list.liquorix /etc/apt/sources.list.d/liquorix-repo.list
-
-update
-
 puts "ADDING KERNEL."
 
-DEFAULT_KERNEL_PKG='
-	linux-image-liquorix
-	libcrypt-dev/trixie
-	libcrypt1/trixie
-'
-
-install_downgrades $DEFAULT_KERNEL_PKG
-
-rm \
-	/etc/apt/sources.list.d/liquorix-repo.list
-
-remove_repo_keys \
-	9352A0B69B72E6DF > /dev/null
-
-update
+08-install-kernel
 
 
 #	Add Plymouth.
-#
-#	The version of Plymouth that is available from Debian requires systemd and udev.
-#	To avoid this requirement, we will use the package from Devuan (daedalus) that only requires udev (eudev).
 
 puts "ADDING PLYMOUTH."
 
-add_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
-
-cp /configs/files/sources.list.devuan.daedalus /etc/apt/sources.list.d/devuan-daedalus-repo.list
-
-update
-
-DEVUAN_PLYMOUTH_PKGS='
-	plymouth/daedalus
-	plymouth-label/daedalus
-	plymouth-x11/daedalus
-'
-
-install $DEVUAN_PLYMOUTH_PKGS
-
-rm \
-	/etc/apt/sources.list.d/devuan-daedalus-repo.list
-
-remove_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
-
-update
+09-install-plymouth
 
 
 #	Adding PolicyKit packages from Devuan.
-#
-#	Since we're using elogind to replace logind, we need to add the matching PolicyKit packages.
-#
-#	Strangely, the complete stack is only available in beowulf but not in chimaera or daedalus.
 
-puts "ADDING POLICYKIT ELOGIND COMPAT."
+puts "ADDING POLICYKIT ELOGIND."
 
-add_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
-
-cp /configs/files/sources.list.devuan.beowulf /etc/apt/sources.list.d/devuan-beowulf-repo.list
-
-update
-
-DEVUAN_POLKIT_PKGS='
-	libpam-elogind/beowulf
-	libpolkit-agent-1-0/beowulf
-	libpolkit-backend-elogind-1-0/beowulf
-	libpolkit-gobject-1-0/beowulf
-	libpolkit-gobject-elogind-1-0/beowulf
-	policykit-1/beowulf
-'
-
-install_downgrades $DEVUAN_POLKIT_PKGS
-
-rm \
-	/etc/apt/sources.list.d/devuan-beowulf-repo.list
-
-remove_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
-
-update
+10-install-policykit
 
 
-#	Add misc. Devuan packages.
-#
-#	The network-manager package that is available in Debian does not have an init script compatible with OpenRC.
-#	so we use the package from Devuan instead.
-#
-#	Prioritize installing packages from daedalus over chimaera, unless the package only exists in ceres.
+#	Adding NetworkManager
 
 puts "ADDING DEVUAN NM PACKAGE."
 
-add_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
-
-cp /configs/files/sources.list.devuan.daedalus /etc/apt/sources.list.d/devuan-daedalus-repo.list
-
-update
-
-DEVUAN_NM_PKGS='
-	network-manager/daedalus
-'
-
-install $DEVUAN_NM_PKGS
-
-rm \
-	/etc/apt/sources.list.d/devuan-daedalus-repo.list
-
-remove_repo_keys \
-	541922FB \
-	61FC752C > /dev/null
-
-update
+11-install-networkmanager
 
 
 #	Add Nitrux meta-packages.
 
 puts "ADDING NITRUX BASE."
 
-NITRUX_BASE_PKGS='
-	base-files=13.1.23+nitrux-legacy
-	nitrux-minimal-legacy
-	nitrux-standard-legacy
-'
-
-NITRUX_HW_PKGS='
-	nitrux-hardware-drivers-legacy
-	broadcom-sta-dkms
-'
-
-install $NITRUX_BASE_PKGS $NITRUX_HW_PKGS
+12-install-nitrux-packages
 
 
 #	Add Nvidia drivers or Nouveau.
-#
-#	The package nouveau-firmware isn't available in Debian but only in Ubuntu.
-#
-#	The Nvidia proprietary driver can't be installed alongside Nouveau.
-
-add_repo_keys \
-	1118213C > /dev/null
-
-cp /configs/files/sources.list.gpu.ppa /etc/apt/sources.list.d/gpu-ppa-repo.list
-
-update
 
 puts "ADDING NVIDIA DRIVERS/NOUVEAU FIRMWARE."
 
-NVIDIA_DRV_PKGS='
-	nvidia-driver-525
-	nvidia-settings
-	nvidia-prime
-'
-
-install $NVIDIA_DRV_PKGS
-
-rm \
-	/etc/apt/sources.list.d/gpu-ppa-repo.list
-
-remove_repo_keys \
-	1118213C > /dev/null
-
-update
+13-install-nvidia-drivers
 
 
 #	Add NX Desktop meta-package.
-#
-#	Use MISC_DESKTOP_PKGS to add packages to test. If tests are positive, add to the appropriate meta-package.
-#
-#	Use the KDE Neon repository to provide the latest stable release of Plasma and KF5.
-
-add_repo_keys \
-	55751E5D > /dev/null
-
-cp /configs/files/sources.list.neon.user /etc/apt/sources.list.d/neon-user-repo.list
-
-update
 
 puts "ADDING NX DESKTOP."
 
-NX_DESKTOP_PKG='
-	nx-desktop-legacy
-	nx-desktop-wayland-legacy
-'
-
-MISC_DESKTOP_PKGS='
-	gstreamer1.0-pipewire
-	inxi
-	kwin-bismuth/trixie
-	kwin-x11/jammy
-	libpipewire-0.3-0
-	libpipewire-0.3-common
-	libpipewire-0.3-modules
-	libspa-0.2-bluetooth
-	libspa-0.2-jack
-	libspa-0.2-modules
-	overlayroot
-	pipewire-bin
-	pipewire-jack
-	pipewire-pulse
-	pipewire-v4l2
-	plasma-workspace-wayland
-	pulseaudio-equalizer-ladspa
-	python3-gi
-	samba
-	smbclient
-	wireplumber
-'
-
-DBGSYM_PACKAGES='
-	kwin-common-dbgsym
-	kwin-x11-dbgsym
-	latte-dock-dbgsym
-	libkwineffects14-dbgsym
-	libkwinglutils14-dbgsym
-	libqt5core5a-dbgsym
-	libqt5dbus5-dbgsym
-	libqt5gui5-dbgsym
-	libqt5qml5-dbgsym
-	libqt5quick5-dbgsym
-	libqt5widgets5-dbgsym
-	print-manager-dbgsym
-	qml-module-org-kde-kwindowsystem-dbgsym
-'
-
-install_downgrades $NX_DESKTOP_PKG $MISC_DESKTOP_PKGS $DBGSYM_PACKAGES
-
-rm \
-	/etc/apt/sources.list.d/neon-user-repo.list
-
-remove_repo_keys \
-	55751E5D > /dev/null
-
-update
+14-install-desktop-session
 
 
 #	Add Calamares.
 
 puts "ADDING CALAMARES INSTALLER."
 
-add_repo_keys \
-	55751E5D > /dev/null
-
-cp /configs/files/sources.list.neon.user /etc/apt/sources.list.d/neon-user-repo.list
-
-update
-
-
-CALAMARES_PKGS='
-	efibootmgr
-	calamares
-	calamares-qml-settings-nitrux
-	dosfstools
-	squashfs-tools
-'
-
-install $CALAMARES_PKGS
-
-rm \
-	/etc/apt/sources.list.d/neon-user-repo.list
-
-remove_repo_keys \
-	55751E5D > /dev/null
-
-update
+15-install-calamares
 
 
 #	Upgrade MESA packages.
 
-puts "UPDATING MESA."
+puts "UPGRADE MESA."
 
-add_repo_keys \
-	A03A4626 > /dev/null
-
-cp /configs/files/sources.list.mesa.git.ppa /etc/apt/sources.list.d/mesa-git-ppa.list
-
-update
-
-MESA_LIBS_PKGS='
-	libdrm-amdgpu1
-	libdrm-common
-	libdrm-intel1
-	libdrm-nouveau2
-	libdrm-radeon1
-	libdrm2
-	libegl-mesa0
-	libgbm1
-	libgl1-mesa-dri
-	libglapi-mesa
-	libglx-mesa0
-	libxatracker2
-	mesa-va-drivers
-	mesa-vdpau-drivers
-	mesa-vulkan-drivers
-'
-
-only_upgrade_force_overwrite $MESA_LIBS_PKGS
-
-rm \
-	/etc/apt/sources.list.d/mesa-git-ppa.list
-
-remove_repo_keys \
-	A03A4626 > /dev/null
-
-update
+16-upgrade-mesa
 
 
 #	Add OpenRC configuration.
-#
-#	Due to how the upstream openrc package "works," we need to put this package at the end of the build process.
-#	Otherwise, we end up with an unbootable system.
-#
-#	See https://github.com/Nitrux/openrc-config/issues/1
 
 puts "ADDING OPENRC CONFIG."
 
-OPENRC_CONFIG='
-	openrc-config
-	nitrux-live-user
-'
+17-install-init-config
 
-install $OPENRC_CONFIG
+
+#	Add Live user configuration.
+
+puts "ADDING LIVE USER CONFIG."
+
+18-install-live-config
 
 
 #	Remove sources used to build the root.
 
 puts "REMOVE BUILD SOURCES."
 
-rm \
-	/etc/apt/preferences \
-	/etc/apt/sources.list.d/* \
-	/usr/share/keyrings/nitrux-repo.gpg \
-	/usr/share/keyrings/nitrux-compat.gpg
-
-update
+19-remove-build-sources
 
 
-#	Update Appstream cache.
-
-clean_all
-update
-appstream_refresh_force
-
-
-#	Unhold initramfs and casper packages.
-
-unhold $INITRAMFS_CASPER_PKGS
-
-
-#	WARNING:
-#	No apt usage past this point.
+####################################
+#	WARNING:						#
+#	No apt usage past this point.	#
+####################################
 
 
 #	Changes specific to this image. If they can be put in a package, do so.
@@ -578,73 +199,33 @@ unhold $INITRAMFS_CASPER_PKGS
 
 puts "ADDING MISC. FIXES."
 
-rm \
-	/etc/default/grub \
-	/etc/casper.conf
-
-cat /configs/files/grub > /etc/default/grub
-cat /configs/files/casper.conf > /etc/casper.conf
-
-rm \
-	/boot/{vmlinuz,initrd.img,vmlinuz.old,initrd.img.old} || true
-
-cat /configs/files/motd > /etc/motd
-
-printf '%s\n' fuse nvidia amdgpu i915 exfat >> /etc/modules
-
-cat /configs/files/adduser.conf > /etc/adduser.conf
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+20-add-misc-fixes
 
 
 #	Generate initramfs.
 
 puts "UPDATING THE INITRAMFS."
 
-cp /configs/files/crypttab-initramfs-hook.sh /usr/share/initramfs-tools/hooks/
-
-update-initramfs -c -k all
+21-update-initramfs
 
 
-#	Before removing dpkg, check the most oversized installed packages.
+#	Check installed packages at end.
 
 puts "SHOW LARGEST INSTALLED PACKAGES."
 
-list_pkgs_size
-list_number_pkgs
-list_installed_pkgs
+list_installed_text_end
+list_installed_origin_text
 
 
-#	WARNING:
-#	No dpkg usage past this point.
+####################################
+#	WARNING:						#
+#	No dpkg usage past this point.	#
+####################################
 
 
 puts "PERFORM MANUAL CHECKS."
 
-ls -lh \
-	/boot \
-	/etc/runlevels/{boot,default,nonetwork,off,recovery,shutdown,sysinit} \
-	/{vmlinuz,initrd.img} \
-	/etc/{init.d,sddm.conf.d} \
-	/usr/lib/dbus-1.0/dbus-daemon-launch-helper \
-	/installed_pkgs.txt
-
-tree \
-	/Applications
-
-stat \
-	/sbin/init \
-	/bin/sh \
-	/bin/dash \
-	/bin/bash
-
-tail -n +1 \
-	/etc/{casper.conf,sddm.conf,modules} \
-	/etc/default/grub \
-	/etc/sddm.conf.d/kde_settings.conf \
-	/etc/environment \
-	/etc/adduser.conf \
-	/etc/modules
+22-manual-checks
 
 
 puts "EXITING BOOTSTRAP."
--- build.sh
+++ build.sh
@@ -1,33 +1,20 @@
 #! /bin/bash
 
+set -e
+
 [ "$__time_traced" ] ||
 	__time_traced=yes exec time "$0" "$@"
 
-#	Exit on errors.
 
-set -xe
+#	Source APT commands as functions.
+#	shellcheck source=/dev/null
+
+source "$PWD"/configs/scripts/others/apt-funcs
 
 
-#	Travis stuff.
+#	Build environment stuff.
 
-XORRISO_PKGS='
-	libburn4
-	libisoburn1
-	libisofs6
-	libjte2
-	mtools
-	sshpass
-	xorriso
-'
-
-GRUB_EFI_PKGS='
-	grub-efi-amd64
-	grub-efi-amd64-signed
-	shim-signed
-'
-
-apt -qq update
-apt -yy install $XORRISO_PKGS $GRUB_EFI_PKGS --no-install-recommends > /dev/null
+bash "$PWD"/configs/scripts/stages/00-install-host-pkgs
 
 
 #	base image URL.
@@ -41,7 +28,7 @@ build_dir=$(mktemp -d)
 iso_dir=$(mktemp -d)
 output_dir=$(mktemp -d)
 
-chmod 755 $build_dir
+chmod 755 "$build_dir"
 
 config_dir=$PWD/configs
 
@@ -55,7 +42,7 @@ hash_url=http://releases.nxos.org/${image%.iso}.md5sum
 #	Prepare the directory where the filesystem will be created.
 
 wget -qO base.tar.xz $base_img_url
-tar xf base.tar.xz -C $build_dir
+tar xf base.tar.xz -C "$build_dir"
 
 
 #	Populate $build_dir.
@@ -66,51 +53,46 @@ chmod +x /bin/runch
 < bootstrap.sh runch \
 	-m configs:/configs \
 	-r /configs \
-	$build_dir \
+	"$build_dir" \
 	bash || :
 
 
 #	Check filesystem size.
 
-du -hs $build_dir
-
-
-#	Remove CI leftovers.
-
-rm -r $iso_dir/home/{travis,Travis} || true
+du -hs "$build_dir"
 
 
 #	Copy the kernel and initramfs to $iso_dir.
 #	BUG: vmlinuz and initrd are not moved to $iso_dir/; they're left at $build_dir/boot
 
-mkdir -p $iso_dir/boot
+mkdir -p "$iso_dir"/boot
 
-cp $(echo $build_dir/boot/vmlinuz* | tr " " "\n" | sort | tail -n 1) $iso_dir/boot/kernel
-cp $(echo $build_dir/boot/initrd*  | tr " " "\n" | sort | tail -n 1) $iso_dir/boot/initramfs
+cp "$(echo "$build_dir"/boot/vmlinuz* | tr " " "\n" | sort | tail -n 1)" "$iso_dir"/boot/kernel
+cp "$(echo "$build_dir"/boot/initrd*  | tr " " "\n" | sort | tail -n 1)" "$iso_dir"/boot/initramfs
 
 
 #	WARNING FIXME BUG: This file isn't copied during the chroot.
 
-mkdir -p $iso_dir/boot/grub/x86_64-efi
-cp /usr/lib/grub/x86_64-efi/linuxefi.mod $iso_dir/boot/grub/x86_64-efi
+mkdir -p "$iso_dir"/boot/grub/x86_64-efi
+cp /usr/lib/grub/x86_64-efi/linuxefi.mod "$iso_dir"/boot/grub/x86_64-efi
 
 
 #	Copy EFI folder to ISO
 
-cp -r EFI/ $iso_dir/
+cp -r EFI/ "$iso_dir"/
 
 
 #	Copy ucode to ISO
 
-cp -r ucode/ $iso_dir/boot/
+cp -r ucode/ "$iso_dir"/boot/
 
 
 #	Compress the root filesystem.
 
 ( while :; do sleep 300; printf ".\n"; done ) &
 
-mkdir -p $iso_dir/casper
-mksquashfs $build_dir $iso_dir/casper/filesystem.squashfs -comp zstd -Xcompression-level 22 -no-progress -b 1048576
+mkdir -p "$iso_dir"/casper
+mksquashfs "$build_dir" "$iso_dir"/casper/filesystem.squashfs -comp zstd -Xcompression-level 22 -no-progress -b 1048576
 
 
 #	Generate the ISO image.
@@ -126,20 +108,20 @@ mkiso \
 	-e \
 	-s "$hash_url" \
 	-r "$(git rev-parse --short=8 HEAD)" \
-	-g $config_dir/files/grub.cfg \
-	-g $config_dir/files/loopback.cfg \
+	-g "$config_dir"/files/grub_files/grub.cfg \
+	-g "$config_dir"/files/grub_files/loopback.cfg \
 	-t grub-theme/nitrux \
-	$iso_dir $output_dir/$image
+	"$iso_dir" "$output_dir"/"$image"
 
 
 #	Calculate the checksum.
 
-md5sum $output_dir/$image > $output_dir/${image%.iso}.md5sum
+md5sum "$output_dir"/"$image" > "$output_dir"/"${image%.iso}".md5sum
 
 
 #	Move files to current directory
 
-mv $output_dir/* $PWD
+mv "$output_dir"/* "$PWD"
 
 
 #	Clean up build directories
